69edd9f773143ccffc63ddd018f6b065e02458f6
Surajit Rana <surajit1602@gmail.com>
2024-06-18 00:47:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        Node revList = reverseList(head);
+        int num = 1;
+        Node current = revList;
+        int carry = num;
+        while (current != null) {
+            int data = (carry + current.data);
+            current.data = data % 10;
+            carry = data / 10;
+            current = current.next;
+        }
+        Node headModified = reverseList(revList);
+        if (carry > 0) {
+            Node rev = new Node(carry);
+            rev.next = headModified;
+            return rev;
+        }
+        return headModified;
+    }
+    
+    private static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+}
