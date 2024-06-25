546673a418dbe303c56b512d430b83df40c190f9
Surajit Rana <surajit1602@gmail.com>
2024-06-25 22:19:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Left Rotate Matrix K times/left-rotate-matrix-k-times.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            int n = Integer.parseInt(S[0]);
+            int m = Integer.parseInt(S[1]);
+            int k = Integer.parseInt(S[2]);
+            int mat[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String S1[] = read.readLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = Integer.parseInt(S1[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans[][] = ob.rotateMatrix(k, mat);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) System.out.print(ans[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for java
+
+class Solution {
+    int[][] rotateMatrix(int k, int mat[][]) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        for (int i = 0; i < rows; i++) {
+            rotateArray(mat[i], k);
+        }
+        return mat;
+    }
+    
+    private void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverseArray(nums, 0, n - 1);
+        reverseArray(nums, 0, n - k - 1);
+        reverseArray(nums, n - k, n - 1);
+    }
+    
+    private void reverseArray(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
