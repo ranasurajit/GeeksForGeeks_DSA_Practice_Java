3fd99fc9e0e13e216e130c754b970c91053ed1d5
Surajit Rana <surajit1602@gmail.com>
2024-06-30 16:38:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete node in Doubly Linked List/delete-node-in-doubly-linked-list.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            Node head = null;
+            Node tail = head;
+
+            for (int i = 0; i < n; i++) {
+                int temp = sc.nextInt();
+                if (head == null) {
+                    head = new Node(temp);
+                    tail = head;
+                } else {
+                    Node newNode = new Node(temp);
+                    tail.next = newNode;
+                    newNode.prev = tail;
+                    tail = newNode;
+                }
+            }
+            int x = sc.nextInt();
+
+            Solution obj = new Solution();
+            Node res = obj.deleteNode(head, x);
+
+            Node.printList(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for doubly Link List Node
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int x){
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    public Node deleteNode(Node head, int x) {
+        if (head == null || (head.prev == null && head.next == null && x == 1)) {
+            return null;
+        }
+        if (x == 1) {
+            head = head.next;
+            return head;
+        }
+        Node prev = null;
+        Node current = head;
+        Node next = head.next;
+        int count = 1;
+        while (current != null && count < x) {
+            prev = current;
+            current = current.next;
+            next = current.next;
+            count++;
+        }
+        prev.next = current.next;
+        if (next != null) {
+            next.prev = prev;   
+        }
+        return head;
+    }
+}
