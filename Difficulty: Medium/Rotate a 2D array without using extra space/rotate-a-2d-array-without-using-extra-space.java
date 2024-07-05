397381d58d7fff16365c3bbdef0af3a3e886df61
Surajit Rana <surajit1602@gmail.com>
2024-07-06 00:00:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate a 2D array without using extra space/rotate-a-2d-array-without-using-extra-space.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] arr = new int[n][n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * n + j]);
+                }
+            }
+
+            new Solution().rotateMatrix(arr, n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    out.print(arr[i][j] + " ");
+                }
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    void rotateMatrix(int arr[][], int n) {
+        for (int[] row : arr) {
+            reverse(row);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+    
+    private void reverse(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int temp = nums[low];
+            nums[low] = nums[high];
+            nums[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
