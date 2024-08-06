4e7c24527625f448935a353936a15455542c6438
Surajit Rana <surajit1602@gmail.com>
2024-08-07 00:55:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+
+            String[] line1 = br.readLine().trim().split(" ");
+            int[] arr1 = new int[line1.length];
+            for (int i = 0; i < line1.length; i++) {
+                arr1[i] = Integer.parseInt(line1[i]);
+            }
+
+            String[] line2 = br.readLine().trim().split(" ");
+            int[] arr2 = new int[line2.length];
+            for (int i = 0; i < line2.length; i++) {
+                arr2[i] = Integer.parseInt(line2[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.kthElement(k, arr1, arr2));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public long kthElement(int k, int arr1[], int arr2[]) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] sorted = new int[len1 + len2];
+        int p = 0;
+        int q = 0;
+        int r = 0;
+        while (p < len1 && q < len2) {
+            if (arr1[p] < arr2[q]) {
+                sorted[r] = arr1[p];
+                p++;
+            } else {
+                sorted[r] = arr2[q];
+                q++;
+            }
+            r++;
+        }
+        while (p < len1) {
+            sorted[r++] = arr1[p++];
+        }
+        while (q < len2) {
+            sorted[r++] = arr2[q++];
+        }
+        return sorted[k - 1];
+    }
+}
