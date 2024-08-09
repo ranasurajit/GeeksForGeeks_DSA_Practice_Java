83145ea32f38c95ffb708bd963780f3b3339191d
Surajit Rana <surajit1602@gmail.com>
2024-08-10 01:16:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -37,22 +37,30 @@ class Solution {
         }
         Node current = head;
         int count = 1;
+        // segment the LinkedList to split it from count (1 to k) and (k + 1 to n - 1)
         while (current != null && count < k) {
             current = current.next;
             count++;
         }
         Node segment = current.next;
         current.next = null;
+        // reverse the segment from count (1 to k)
         Node segment1 = reverseLL(head);
         current = segment1;
         while (current.next != null) {
             current = current.next;
         }
+        // reverse the segment from count (k + 1 to n - 1)
         Node segment2 = reverseLL(segment);
+        // link both segments
         current.next = segment2;
+        // finally reverse the entire linked segment1 and segment2
         return reverseLL(segment1);
     }
     
+    /**
+     * reverse LinkedList and return the head
+     */
     private Node reverseLL(Node head) {
         if (head == null || head.next == null) {
             return head;
