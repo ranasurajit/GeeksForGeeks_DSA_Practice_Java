805531cb39a2e7d17815db1f0fffeeed32d529bb
Surajit Rana <surajit1602@gmail.com>
2024-08-18 03:50:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.java
+++ Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.java
@@ -25,16 +25,19 @@ class GFG {
 class Solution {
     public boolean canSplit(int arr[]) {
         int n = arr.length;
+        // pre-processing prefix sum
         long[] prefix = new long[n];
         prefix[0] = arr[0];
         for (int i = 1; i < n; i++) {
             prefix[i] = prefix[i - 1] + arr[i];
         }
+        // pre-processing suffix sum
         long[] suffix = new long[n];
         suffix[n - 1] = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             suffix[i] = suffix[i + 1] + arr[i];
         }
+        // to have a split ith element of prefix and (i + 1)th element of suffix should be same
         for (int i = 0; i < n - 1; i++) {
             if (prefix[i] == suffix[i + 1]) {
                 return true;
