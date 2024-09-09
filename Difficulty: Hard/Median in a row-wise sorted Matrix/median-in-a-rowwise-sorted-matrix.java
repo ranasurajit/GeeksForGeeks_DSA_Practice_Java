4a43e42adf33a7f67133c642215c77405f409ba4
Surajit Rana <surajit1602@gmail.com>
2024-09-09 22:01:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -37,54 +37,83 @@ class GFG
 //User function Template for Java
 
 class Solution {
+    /**
+     * TC: O(R) + O(Rlog(C)) ~ O(Rlog(C))
+     * SC: O(1)
+     * 
+     * @param matrix
+     * @param R
+     * @param C
+     * @return
+     */
     int median(int matrix[][], int R, int C) {
+        // finding range
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+        for (int i = 0; i < R; i++) { // TC: O(R)
+            low = Math.min(low, matrix[i][0]);
+            high = Math.max(high, matrix[i][C - 1]);
+        }
         /**
          * R and C are odd i.e. number of elements is R x C which is odd
          * so we have only 1 element as median
          */
-        int median = 0;
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
-        for (int i = 0; i < R; i++) {
-            low = Math.min(low, matrix[i][0]);
-            high = Math.max(high, matrix[i][C - 1]);
-        }
-        int n = (R * C) / 2;
-        while (low <= high) {
+        int targetIndex = (R * C) / 2;
+        // Applying Binary Search
+        while (low <= high) { // TC: O(log(Range)) where Range = high - low i.e. constant
             int mid = low + (high - low) / 2;
-            int lessThanEquals = getNumElementsLessThan(matrix, mid);
-            if (lessThanEquals <= n) {
+            // find number of elements less than mid value in matrix
+            int lessThanEquals = getLessThanEquals(matrix, R, C, mid);
+            if (lessThanEquals <= targetIndex) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return low;
+        return low;     
     }
     
-    private int getNumElementsLessThan(int[][] matrix, int k) {
-        int upperBound = 0;
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        for (int i = 0; i < rows; i++) {
-            upperBound += getUpperBound(matrix[i], k, rows, cols);
+    /**
+     * TC: O(Rlog(C))
+     * SC: O(1)
+     * 
+     * @param matrix
+     * @param R
+     * @param C
+     * @param mid
+     * @return
+     */
+    private int getLessThanEquals(int[][] matrix, int R, int C, int mid) {
+        // finding sum of occurences of mid value in each row
+        int count = 0;
+        for (int i = 0; i < R; i++) {
+            count += getUpperBound(matrix[i], mid);
         }
-        return upperBound;
+        return count;
     }
-    
-    private int getUpperBound(int[] row, int k, int rows, int cols) {
+
+    /**
+     * TC: O(log(C))
+     * SC: O(1)
+     * 
+     * @param row
+     * @param k
+     * @return
+     */
+    private int getUpperBound(int[] row, int k) {
         int low = 0;
-        int high = cols - 1;
-        int index = cols;
+        int high = row.length - 1;
+        int lessThan = row.length;
+        // Applying Binary Search
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (row[mid] > k) {
-                index = mid;
+                lessThan = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return index;
+        return lessThan;
     }
 }
