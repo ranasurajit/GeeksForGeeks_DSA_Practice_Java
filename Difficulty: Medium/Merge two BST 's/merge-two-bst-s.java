f60fce54ebb042839b71d9756dd53305fe88257c
Surajit Rana <surajit1602@gmail.com>
2024-09-30 11:26:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -0,0 +1,160 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the curr node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the curr node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the curr node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+
+            s = br.readLine();
+            Node root2 = buildTree(s);
+
+            Solution T = new Solution();
+            List<Integer> ans = T.merge(root1, root2);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+
+*/
+class Solution {
+    /**
+     * TC: O(M + N)
+     * SC: O(M + N)
+     */
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1, Node root2) {
+        List<Integer> result = new ArrayList<Integer>(); 
+        List<Integer> list1 = new ArrayList<Integer>(); // SC: O(M)
+        List<Integer> list2 = new ArrayList<Integer>(); // SC: O(N)
+        inorder(root1, list1);
+        inorder(root2, list2);
+        // Merging both sorted arrays
+        int m = list1.size();
+        int n = list2.size();
+        int p = 0; // pointer for list1
+        int q = 0; // pointer for list2
+        while (p < m && q < n) { // TC: O(M + N)
+            if (list1.get(p) < list2.get(q)) {
+                result.add(list1.get(p));
+                p++;
+            } else {
+                result.add(list2.get(q));
+                q++;
+            }
+        }
+        while (p < m) {
+            result.add(list1.get(p));
+            p++;
+        }
+        while (q < n) {
+            result.add(list2.get(q));
+            q++;
+        }
+        return result;
+    }
+    
+    /**
+     * Inorder traversal of BST is always sorted
+     * Inorder - Left Node Right
+     */
+    private void inorder(Node root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.data);
+        inorder(root.right, list);
+    }
+}
