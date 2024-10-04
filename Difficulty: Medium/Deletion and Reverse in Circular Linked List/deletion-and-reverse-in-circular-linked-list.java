3b1b08179fc9d510778683088224530a70964c5b
Surajit Rana <surajit1602@gmail.com>
2024-10-04 16:51:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Deletion and Reverse in Circular Linked List/deletion-and-reverse-in-circular-linked-list.java
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class LinkedList {
+    // Function to print nodes in a given circular linked list
+    static void printList(Node head) {
+        if (head == null) {
+            System.out.println("empty");
+            return;
+        }
+        Node temp = head;
+        do {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int key = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            tail.next = head; // Make the list circular
+            Solution ob = new Solution();
+            Node current = ob.deleteNode(head, key);
+            Node rev = ob.reverse(current);
+            printList(rev);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data=d;next=null;
+    }
+}*/
+
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    // Function to reverse a circular linked list
+    Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        do {
+            Node temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        } while (current != head);
+        head.next = prev;
+        return prev;
+    }
+
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    // Function to delete a node from the circular linked list
+    Node deleteNode(Node head, int key) {
+        if (head == null || (head.next == head && head.data == key)) {
+            return null;
+        }
+        if (head.data == key) {
+            // delete head
+            Node current = head;
+            while (current.next != head) {
+                current = current.next;
+            }
+            current.next = head.next;
+            return current.next;
+        } else {
+            // delete any other node
+            Node current = head;
+            do {
+                if (current.next.data == key) {
+                    current.next = current.next.next;
+                    break;
+                }
+                current = current.next;
+            } while (current != head);
+            return head;
+        }
+    }
+}
