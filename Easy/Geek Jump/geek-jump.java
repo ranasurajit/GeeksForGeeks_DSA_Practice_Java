46f72860aea6b5dc6a852472ece052d9fb11e68e
surajitrana <surajit1602@gmail.com>
2024-10-09 01:24:39 +0530

    fix[folder-restructure]: folder modified

--- Easy/Geek Jump/geek-jump.java
+++ Easy/Geek Jump/geek-jump.java
@@ -30,23 +30,26 @@ class GFG{
 
 //User function Template for Java
 
-class Solution{
+class Solution {
     /**
      * TC: O(N)
      * SC: O(N)
      */
-    public int minimumEnergy(int arr[],int N){
+    public int minimumEnergy(int arr[],int N) {
+        if (N == 1) {
+            return 0;
+        }
         int[] dp = new int[N]; // SC: O(N)
-        dp[0] = 0;
-        // dp[1] = Math.abs(arr[1] - arr[0]) + dp[0];
-        // dp[2] = Math.min(Math.abs(arr[2] - arr[1]) + dp[1], Math.abs(arr[2] - arr[0]) + dp[0]);
-        for (int i = 1; i < N; i++) { // TC: O(N)
-            int x = Math.abs(arr[i] - arr[i - 1]) + dp[i - 1];
-            int y = Integer.MAX_VALUE;
-            if (i > 1) {
-                y = Math.abs(arr[i] - arr[i - 2]) + dp[i - 2];
-            }
-            dp[i] = Math.min(x, y);
+        dp[0] = 0; // min energy to climb 0th stair
+        // min energy to climb 1st stair (can jump only 1 step)
+        dp[1] = Math.abs(arr[1] - arr[0]) + dp[0];
+        /* 
+         * dp[2] = Math.min(Math.abs(arr[2] - arr[1]) + dp[1],
+         *                  Math.abs(arr[2] - arr[0]) + dp[0]);
+         */
+        for (int i = 2; i < N; i++) { // TC: O(N)
+            dp[i] = Math.min(Math.abs(arr[i] - arr[i - 1]) + dp[i - 1], 
+                             Math.abs(arr[i] - arr[i - 2]) + dp[i - 2]);
         }
         return dp[N - 1];
     }
--- Medium/Minimal Cost/minimal-cost.java
+++ Medium/Minimal Cost/minimal-cost.java
@@ -9,20 +9,21 @@ import java.util.*;
 // User function Template for Java
 
 class Solution {
+    /**
+     * TC: O(N x K)
+     * SC: O(N)
+     */
     public int minimizeCost(int k, int arr[]) {
         int n = arr.length;
-        int[] dp = new int[n];
-        dp[0] = 0; // cost at 1st stone is zero as Geek is standing there
-        // dp[1] = arr[1] - arr[0];
-        // dp[2] = Math.abs(arr[2] - arr[1]) + dp[1];
-        // dp[i] needs to be found where dp[i] = min cost to reach at ith stone
-        // ith stone can be reached from (i - 1)th, (i - 2)th ... (i - k)th stone
-        for (int i = 1; i < n; i++) {
-            int cost = Integer.MAX_VALUE;
-            for (int j = 1; j <= k && j <= i; j++) {
-                cost = Math.min(cost, Math.abs(arr[i] - arr[i - j]) + dp[i - j]);
+        int[] dp = new int[n]; // SC: O(N)
+        dp[0] = 0; // best (min) cost till index 0
+        dp[1] = Math.abs(arr[1] - arr[0]) + dp[0]; // best (min) cost till index 1
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            int minCost = Integer.MAX_VALUE;
+            for (int j = 1; j <= Math.min(i, k); j++) { // TC: O(K)
+                minCost = Math.min(minCost, Math.abs(arr[i] - arr[i - j]) + dp[i - j]);
             }
-            dp[i] += cost;
+            dp[i] = minCost; // best (min) cost till index i
         }
         return dp[n - 1];
     }
