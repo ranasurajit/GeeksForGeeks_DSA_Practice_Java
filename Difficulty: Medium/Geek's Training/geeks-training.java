844b3f820de5de92bf2acde4b30a7897ab51d562
Surajit Rana <surajit1602@gmail.com>
2024-11-11 11:17:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.java
+++ Difficulty: Medium/Geek's Training/geeks-training.java
@@ -31,7 +31,85 @@ System.out.println("~");
 // User function Template for Java
 
 class Solution {
+    /**
+     * Using Space Optimization
+     * TC: O(3 x 4 x N)
+     * SC: O(4) ~ O(1)
+     * 
+     * @param arr
+     * @param N
+     * @return
+     */
     public int maximumPoints(int arr[][], int N) {
+        /**
+         * We need to store a prev array of size 4 (3 tasks (0, 1, 2) + 1 No task (3))
+         */
+        int[] prev = new int[4]; // SC: O(4)
+        prev[0] = Math.max(arr[0][1], arr[0][2]); // if task 0 was chosen as last task
+        prev[1] = Math.max(arr[0][0], arr[0][2]); // if task 1 was chosen as last task
+        prev[2] = Math.max(arr[0][0], arr[0][1]); // if task 2 was chosen as last task
+        prev[3] = Math.max(arr[0][0], Math.max(arr[0][1], arr[0][2])); // if task 2 was chosen as last task
+
+        for (int day = 1; day < N; day++) { // TC: O(N)
+            int[] current = new int[4]; // SC: O(4)
+            for (int last = 0; last <= 3; last++) { // TC: O(4)
+                current[last] = 0;
+                for (int task = 0; task <= 2; task++) { // TC: O(3)
+                    if (task != last) {
+                        int points = arr[day][task] + prev[task];
+                        current[last] = Math.max(current[last], points);
+                    }
+                }
+            }
+            prev = current;
+        }
+        return prev[3];
+    }
+    
+    /**
+     * Using Tabulation
+     * TC: O(3 x 4 x N)
+     * SC: O(4 x N)
+     * 
+     * @param arr
+     * @param N
+     * @return
+     */
+    public int maximumPointsTabulation(int arr[][], int N) {
+        /**
+         * We need a dp array of size N x 4 (N days and 3 tasks (0, 1, 2) + 1 No task
+         * (3))
+         */
+        int[][] dp = new int[N][4]; // SC: O(4 x N)
+        dp[0][0] = Math.max(arr[0][1], arr[0][2]); // if task 0 was chosen as last task
+        dp[0][1] = Math.max(arr[0][0], arr[0][2]); // if task 1 was chosen as last task
+        dp[0][2] = Math.max(arr[0][0], arr[0][1]); // if task 2 was chosen as last task
+        dp[0][3] = Math.max(arr[0][0], Math.max(arr[0][1], arr[0][2])); // if task 2 was chosen as last task
+
+        for (int day = 1; day < N; day++) { // TC: O(N)
+            for (int last = 0; last <= 3; last++) { // TC: O(4)
+                dp[day][last] = 0;
+                for (int task = 0; task <= 2; task++) { // TC: O(3)
+                    if (task != last) {
+                        int points = arr[day][task] + dp[day - 1][task];
+                        dp[day][last] = Math.max(dp[day][last], points);
+                    }
+                }
+            }
+        }
+        return dp[N - 1][3];
+    }
+    
+    /**
+     * Using Memoization
+     * TC: O(3 x 4 x N)
+     * SC: O(4 x N + R), where R is the Recursive stack space where R = N
+     * 
+     * @param arr
+     * @param N
+     * @return
+     */
+    public int maximumPointsMemoization(int arr[][], int N) {
         /**
          * We need a dp array of size N x 4 (N days and 3 tasks (0, 1, 2) + 1 No task
          * (3))
