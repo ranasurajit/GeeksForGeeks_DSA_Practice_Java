21087be3a963a82ae3f683d6c063b9c71bd31c26
Surajit Rana <surajit1602@gmail.com>
2024-11-15 00:33:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.java
+++ Difficulty: Easy/Square Root/square-root.java
@@ -23,14 +23,40 @@ this function*/
 // x: element to find square root
 class Solution {
     /**
-     * Brute-Force Approach
-     * TC: O(N)
+     * Optimal Approach
+     * 
+     * TC: O(log(N))
      * SC: O(1)
      * 
      * @param n
      * @return
      */
     int floorSqrt(int n) {
+        int max = Integer.MIN_VALUE;
+        int low = 0;
+        int high = n;
+        while (low <= high) { // TC: O(log(N))
+            int mid = low + (high - low) / 2;
+            if (calculateProduct(mid) <= n) { // TC: O(1)
+                max = Math.max(max, mid);
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return max;
+    }
+    
+    /**
+     * Brute-Force Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     * 
+     * @param n
+     * @return
+     */
+    int floorSqrtBruteForce(int n) {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i <= n; i++) { // TC: O(N)
             if (calculateProduct(i) <= n) {
