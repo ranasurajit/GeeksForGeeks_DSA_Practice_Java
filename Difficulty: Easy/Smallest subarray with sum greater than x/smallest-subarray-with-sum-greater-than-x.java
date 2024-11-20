c60f09ef15911b99d06ae8e55547f1132ed41a9c
Surajit Rana <surajit1602@gmail.com>
2024-11-20 18:56:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.java
+++ Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.java
@@ -32,10 +32,46 @@ System.out.println("~");
 
 class Solution {
     /**
+     * Using Deque
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public static int smallestSubWithSum(int x, int[] arr) {
+        int n = arr.length;
+        int minLength = Integer.MAX_VALUE;
+        long sum = 0L;
+        ArrayDeque<Pair> deque = new ArrayDeque<Pair>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            sum += arr[i];
+            if (sum > x) {
+                // capturing minLength when sum becomes > x
+                minLength = Math.min(minLength, i + 1);
+            }
+            while (!deque.isEmpty() && sum - deque.peekFirst().sum > x) {
+                minLength = Math.min(minLength, i - deque.pollFirst().index);
+            }
+            deque.addLast(new Pair(i, sum));
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+    
+    static class Pair {
+        int index;
+        long sum;
+        public Pair (int index, long sum) {
+            this.index = index;
+            this.sum = sum;
+        }
+    }
+
+    /**
+     * Using Two Pointers
+     * 
      * TC: O(N)
      * SC: O(1)
      */
-    public static int smallestSubWithSum(int x, int[] arr) {
+    public static int smallestSubWithSumUsingTwoPointers(int x, int[] arr) {
         int n = arr.length;
         int minLength = Integer.MAX_VALUE;
         long sum = 0L;
