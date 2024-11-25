156fa6152d99226319b9e25e325d3e733f235c2e
Surajit Rana <surajit1602@gmail.com>
2024-11-25 22:48:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.lowerBound(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    int lowerBound(int[] arr, int target) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (arr[i] >= target) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
