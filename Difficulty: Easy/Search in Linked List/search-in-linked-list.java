fd7e4100432a246465ad6eca114383bb58d483d5
Surajit Rana <surajit1602@gmail.com>
2024-12-03 23:56:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+
+} class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            boolean ans = g.searchKey(n, head, key);
+            System.out.println(ans ? "1" : "0");
+            // printList(head);
+            t--;
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    /**
+     * TC: O(N)
+     * SC: O(1)
+     */
+    static boolean searchKey(int n, Node head, int key) {
+        if (head == null) {
+            return false;
+        }
+        Node current = head;
+        while (current != null) { // TC: O(N)
+            if (current.data == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
