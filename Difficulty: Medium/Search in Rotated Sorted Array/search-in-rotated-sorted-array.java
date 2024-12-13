212fe059689d70d4ad2dac242b666430c11eb7ed
Surajit Rana <surajit1602@gmail.com>
2024-12-14 01:24:37 +0530

    Update search-in-rotated-sorted-array.java

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -40,6 +40,11 @@ public class GFG {
 
 class Solution {
     /**
+     * Using Binary Search
+     * 
+     * Intuition - To shrink the search space by finding 
+     * the sorted portion of the rotated sorted array
+     * 
      * TC: O(log(N))
      * SC: O(1)
      */
@@ -47,22 +52,26 @@ class Solution {
         int n = arr.length;
         int low = 0;
         int high = n - 1;
-        while (low <= high) {
+        while (low <= high) {                // TC: O(log(N))
             int mid = low + (high - low) / 2;
             if (arr[mid] == key) {
                 return mid;
-            } else if (arr[mid] >= arr[low]) {
-                // left part sorted
-                if (key >= arr[low] && key < arr[mid]) {
+            } else if (arr[low] <= arr[mid]) {
+                // left part is sorted
+                if (arr[low] <= key && key <= arr[mid]) {
+                    // key is probably present here so shrink the high
                     high = mid - 1;
                 } else {
+                    // key is not present in left part so shrink the low
                     low = mid + 1;
                 }
             } else {
                 // right part is sorted
-                if (key > arr[mid] && key <= arr[high]) {
+                if (arr[mid] <= key && key <= arr[high]) {
+                    // key is probably present here so shrink the low
                     low = mid + 1;
                 } else {
+                    // k is not present in right part so shrink the high
                     high = mid - 1;
                 }
             }
