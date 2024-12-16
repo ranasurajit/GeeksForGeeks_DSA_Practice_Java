cb89bc35033b7bdb2b429097e498c0f89bf26c10
Surajit Rana <surajit1602@gmail.com>
2024-12-17 00:57:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -33,20 +33,20 @@ class GFG {
 class Solution {
     /**
      * Optimal Approach (Using Binary Search)
-     *
-     * TC: O(N x log(N) + N x log(K)), where K = Max(stalls) - Min(stalls)
+     * 
+     * TC: O(N x log(N) + N x log(K))
      * SC: O(1)
      */
     public static int aggressiveCows(int[] stalls, int k) {
         int n = stalls.length;
         Arrays.sort(stalls); // TC: O(N x log(N))
         int low = 1;
-        int high = stalls[n - 1] - stalls[0]; // high can be max distance between extremes
+        int high = stalls[n - 1] - stalls[0];
         int maxDist = 0;
-        while (low <= high) { // TC: O(log(K)), where K = Max(stalls) - Min(stalls)
+        while (low <= high) { // TC: O(log(K)), where K = Max(stalls) - 1
             int mid = low + (high - low) / 2;
-            if (canPlaceAllCows(mid, stalls, k, n)) { // TC: O(N)
-                maxDist = Math.max(maxDist, mid);
+            if (canAllCowsPlaced(mid, stalls, k, n)) { // TC: O(N)
+                maxDist = mid;
                 low = mid + 1;
             } else {
                 high = mid - 1;
@@ -57,18 +57,18 @@ class Solution {
     
     /**
      * Brute-Force Approach (Using Linear Search)
-     *
-     * TC: O(N x log(N) + K x N), where K = Max(stalls) - Min(stalls)
+     * 
+     * TC: O(N x log(N) + K x N) ~ O(K x N)
      * SC: O(1)
      */
     public static int aggressiveCowsBruteForce(int[] stalls, int k) {
         int n = stalls.length;
         Arrays.sort(stalls); // TC: O(N x log(N))
         int low = 1;
-        int high = stalls[n - 1] - stalls[0]; // high can be max distance between extremes
+        int high = stalls[n - 1] - stalls[0];
         int maxDist = 0;
-        for (int i = low; i <= high; i++) { // TC: O(K), where K = Max(stalls) - Min(stalls)
-            if (canPlaceAllCows(i, stalls, k, n)) { // TC: O(N)
+        for (int i = low; i <= high; i++) { // TC: O(K), where K = Max(stalls) - 1
+            if (canAllCowsPlaced(i, stalls, k, n)) { // TC: O(N)
                 maxDist = Math.max(maxDist, i);
             }
         }
@@ -79,15 +79,15 @@ class Solution {
      * TC: O(N)
      * SC: O(1)
      */
-    private static boolean canPlaceAllCows(int dist, int[] stalls, int k, int n) {
-        int cowsPlaced = 1;
-        int lastPlaced = 0;
-        for (int i = 1; i < n; i++) {
-            if (stalls[i] - stalls[lastPlaced] >= dist) {
-                lastPlaced = i;
-                cowsPlaced++;
+    private static boolean canAllCowsPlaced(int minDist, int[] stalls, int k, int n) {
+        int placed = 1;
+        int lastPlacedIndex = 0;
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            if (stalls[i] - stalls[lastPlacedIndex] >= minDist) {
+                placed++;
+                lastPlacedIndex = i;
             }
         }
-        return cowsPlaced >= k;
+        return placed >= k;
     }
 }
