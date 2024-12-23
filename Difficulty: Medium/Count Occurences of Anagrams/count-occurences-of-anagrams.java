cf9ab7231c46db30cee70088f95ada776b95b393
Surajit Rana <surajit1602@gmail.com>
2024-12-24 00:56:40 +0530

    Update count-occurences-of-anagrams.java

--- Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
+++ Difficulty: Medium/Count Occurences of Anagrams/count-occurences-of-anagrams.java
@@ -29,25 +29,58 @@ System.out.println("~");
 
 class Solution {
     /**
+     * k = size of window (String pat)
+     * used a freqency array chars to store frequency of all characters in String 'pat'
+     * 
+     * Took two pointers i and j = 0 and increment j till window size of 'k' is
+     * reached
+     * 
+     * decrement frequency in 'chars' array for index at j
+     * 
+     * Window size: (j - i + 1)
+     * 
+     * when window size < k, then keep increasing j
+     * when window size = k, 
+     *  1. check if frequency of all elements in 'chars' array = 0, then increment count
+     *  2. increase the frequency of ith index in 'chars' array to remove it from 
+     *     next sliding windows
+     * 
+     * maintain this, by incrementing both i an j
+     * 
      * TC: O(N + K)
-     * SC: O(1)
+     * SC: O(26) ~ O(1)
+     * 
+     * @param pat
+     * @param txt
+     * @return
      */
     int search(String pat, String txt) {
         int k = pat.length();
+        int n = txt.length();
         int[] chars = new int[26];
         for (int i = 0; i < k; i++) { // TC: O(K)
             chars[pat.charAt(i) - 'a']++;
         }
-        int n = txt.length();
         int count = 0;
-        for (int i = 0, j = 0; j < n; j++) { // TC: O(N)
+        // window size is constant = m (pattern size);
+        int i = 0; // pointer for start of sliding window
+        int j = 0; // pointer for end of sliding window
+        while (j < n) { // TC: O(N)
             chars[txt.charAt(j) - 'a']--;
-            if (j - i + 1 == k) {
-                if (containsAllZeros(chars)) {
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                if (isEmpty(chars)) { // TC: O(26)
                     count++;
                 }
+                /*
+                 * removing the ith count from window by increasing
+                 * the frequency of the char at index i
+                 */
                 chars[txt.charAt(i) - 'a']++;
+                // move the sliding window
                 i++;
+                j++;
             }
         }
         return count;
@@ -56,10 +89,13 @@ class Solution {
     /**
      * TC: O(26)
      * SC: O(1)
+     * 
+     * @param chars
+     * @return
      */
-    private boolean containsAllZeros(int[] chars) {
-        for (int ch : chars) {
-            if (ch != 0) {
+    private static boolean isEmpty(int[] chars) {
+        for (int i = 0; i < 26; i++) {
+            if (chars[i] != 0) {
                 return false;
             }
         }
