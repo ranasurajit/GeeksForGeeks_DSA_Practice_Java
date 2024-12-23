4ffcf4778a7c6ed6cadf2aaba90eaf53985c193d
Surajit Rana <surajit1602@gmail.com>
2024-12-23 23:34:57 +0530

    Update first-negative-in-every-window-of-size-k.java

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.java
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.java
@@ -10,28 +10,53 @@ class Solution {
 
     // Function to find the first negative integer in every window of size k
     /**
+     * Took two pointers i and j = 0 and increment j till window size of 'k' is
+     * reached
+     * 
+     * add arr[i] < 0 to a Deque as we can add and remove from both ends of it
+     * 
+     * Window size: (j - i + 1)
+     * 
+     * when window size < k, then keep increasing j
+     * when window size = k, 
+     *  1. if deque is empty add 0 to result
+     *  2. if not, add the 1st element of deque to result, check if arr[i] == deque's 
+     *     1st element then remove from deque to move the sliding window
+     * 
+     * maintain this, by incrementing both i an j
+     * 
      * TC: O(N)
      * SC: O(N)
+     * @param arr
+     * @param k
+     * @return
      */
     static List<Integer> FirstNegativeInteger(int arr[], int k) {
         int n = arr.length;
+        int i = 0; // pointer for start of sliding window
+        int j = 0; // pointer for end of sliding window
         List<Integer> result = new ArrayList<Integer>();
-        ArrayDeque<Integer> negDeque = new ArrayDeque<Integer>(); // SC: O(N)
-        for (int i = 0, j = 0; j < n; j++) { // TC: O(N)
+        ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+        while (j < n) { // TC: O(N)
             if (arr[j] < 0) {
-                negDeque.addLast(arr[j]);
+                deque.addLast(arr[j]);
             }
-            if (j - i + 1 == k) {
-                // window size is valid
-                if (!negDeque.isEmpty()) {
-                    result.add(negDeque.peekFirst());
-                } else {
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                // window size is reached
+                if (deque.isEmpty()) {
                     result.add(0);
+                } else {
+                    // deque has atleast 1 negative number present
+                    result.add(deque.peekFirst());
+                    if (deque.peekFirst() == arr[i]) {
+                        deque.pollFirst();
+                    }
                 }
-                if (!negDeque.isEmpty() && arr[i] < 0) {
-                    negDeque.pollFirst();
-                }
+                // maintain the sliding window and move it further by 1 element
                 i++;
+                j++;
             }
         }
         return result;
