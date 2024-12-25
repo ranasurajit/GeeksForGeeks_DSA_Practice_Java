560e95b449ce3821e8661ad42b3fed10e17f4d06
Surajit Rana <surajit1602@gmail.com>
2024-12-25 18:23:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -24,36 +24,50 @@ System.out.println("~");
 // User function Template for Java
 
 class Solution {
+    /**
+     * Took two pointers i and j = 0
+     * 
+     * Window size: (j - i + 1)
+     * 
+     * when HashMap size < k, j++
+     * when HashMap size = k, compare and store the maximum window size (result),
+     * j++
+     * till HashMap size > k, then start removing elements from index 'i'
+     * and update its count/remove key from HashMap, j++
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     *
+     * @param s
+     * @param k
+     * @return
+     */
     public int longestkSubstr(String s, int k) {
         int n = s.length();
         int i = 0;
         int j = 0;
-        Map<Character, Integer> unique = new HashMap<Character, Integer>();
+        Map<Character, Integer> unique = new HashMap<Character, Integer>(); // SC: O(N)
         int maxLength = Integer.MIN_VALUE;
-        while (j < n) {
-            // add the character and it's count in HashMap 'unique' 
+        while (j < n) { // TC: O(N)
+            // add the character and it's count in HashMap 'unique'
             unique.put(s.charAt(j), unique.getOrDefault(s.charAt(j), 0) + 1);
-            if (unique.size() < k) {
-                j++;
-            } else if (unique.size() == k) {
-                maxLength = Math.max(maxLength, j - i + 1);
-                j++;
-            } else if (unique.size() > k) {
-                while (unique.size() > k) {
-                    // remove i index character from HashMap 'unique' 
-                    unique.put(s.charAt(i), unique.getOrDefault(s.charAt(i), 0) - 1);
-                    if (unique.get(s.charAt(i)) == 0) {
-                        /*
-                         * character count has become zero after decreasing frequency
-                         * so remove it from HashMap 'unique'
-                         */
-                        unique.remove(s.charAt(i));
-                    }
-                    // move i index to next to shrink the sliding window
-                    i++;
+            while (unique.size() > k) {
+                // remove i index character from HashMap 'unique'
+                unique.put(s.charAt(i), unique.getOrDefault(s.charAt(i), 0) - 1);
+                if (unique.get(s.charAt(i)) == 0) {
+                    /*
+                     * character count has become zero after decreasing frequency
+                     * so remove it from HashMap 'unique'
+                     */
+                    unique.remove(s.charAt(i));
                 }
-                j++;
+                // move i index to next to shrink the sliding window
+                i++;
             }
+            if (unique.size() == k) {
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+            j++;
         }
         return maxLength == Integer.MIN_VALUE ? -1 : maxLength;
     }
