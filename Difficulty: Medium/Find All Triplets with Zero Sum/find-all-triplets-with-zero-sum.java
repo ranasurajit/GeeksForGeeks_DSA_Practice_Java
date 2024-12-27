a3bf5ffc843f24dc19beb161494223804db6f02a
Surajit Rana <surajit1602@gmail.com>
2024-12-28 01:45:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.java
+++ Difficulty: Medium/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.java
@@ -8,10 +8,43 @@ import java.util.*;
 // User function Template for Java
 class Solution {
     /**
+     * TC: O(N ^ 2 + N) ~ O(N ^ 2)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public List<List<Integer>> findTriplets(int[] arr) {
+        List<List<Integer>> triplets = new ArrayList<List<Integer>>();
+        HashMap<Integer, List<int[]>> map = new HashMap<Integer, List<int[]>>(); // SC: O(N)
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {             // TC: O(N)
+            for (int j = i + 1; j < n; j++) {         // TC: O(N)
+                int sum = arr[i] + arr[j];
+                if (!map.containsKey(sum)) {
+                    map.put(sum, new ArrayList<int[]>());
+                }
+                map.get(sum).add(new int[] { i, j });
+            }
+        }
+        HashSet<List<Integer>> hs = new HashSet<List<Integer>>();     // SC: O(N)
+        for (int i = 0; i < n; i++) {                 // TC: O(N)
+            if (map.containsKey(-1 * arr[i])) {
+                List<int[]> indicesList = map.get(-1 * arr[i]);
+                for (int[] indices : indicesList) {
+                    if (i != indices[0] && i != indices[1]) {
+                        List<Integer> list = Arrays.asList(i, indices[0], indices[1]);
+                        Collections.sort(list);
+                        hs.add(list);
+                    }
+                }
+            }
+        }
+        return new ArrayList<List<Integer>>(hs);
+    }
+    
+    /**
      * TC: O(N ^ 3)
      * SC: O(1)
      */
-    public List<List<Integer>> findTriplets(int[] arr) {
+    public List<List<Integer>> findTripletsBruteForce(int[] arr) {
         List<List<Integer>> triplets = new ArrayList<List<Integer>>();
         int n = arr.length;
         for (int i = 0; i < n; i++) {                 // TC: O(N)
