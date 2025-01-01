b0b2d6ced43077ec62c9e82f8d158c9407d5eb9e
Surajit Rana <surajit1602@gmail.com>
2025-01-01 13:10:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
+++ Difficulty: Medium/Print Anagrams Together/print-anagrams-together.java
@@ -4,22 +4,33 @@ import java.util.*;
 
 // } Driver Code Ends
 class Solution {
+    /**
+     * TC: O(K x N) ~ O(N)
+     * SC: O(N)
+     * where K = length of word in String[] arr and as per constraint
+     * 1 <= K <=10
+     */
     public ArrayList<ArrayList<String>> anagrams(String[] arr) {
         ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        // SC: O(N)
         Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
-        for (String word : arr) {
-            String key = uniqueKey(word);
-            if (!map.containsKey(key)) {
+        for (String word : arr) {         // TC: O(N)
+            String key = uniqueKey(word); // TC: O(K)
+            if (!map.containsKey(key)) {  
                 map.put(key, new ArrayList<String>());
             }
             map.get(key).add(word);
         }
-        for (String key : map.keySet()) {
+        for (String key : map.keySet()) { // TC: O(N)
             result.add(map.get(key));
         }
         return result;
     }
     
+    /**
+     * TC: O(K), K = length of word
+     * SC: O(26) ~ O(1)
+     */
     private String uniqueKey(String word) {
         int[] chars = new int[26];
         for (char ch : word.toCharArray()) {
