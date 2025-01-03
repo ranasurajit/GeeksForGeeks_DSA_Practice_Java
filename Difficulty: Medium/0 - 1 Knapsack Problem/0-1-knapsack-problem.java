42d7560b6afb2211378f84d1881e748f004c8334
Surajit Rana <surajit1602@gmail.com>
2025-01-03 21:28:05 +0530

    Update 0-1-knapsack-problem.java

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,76 +1,155 @@
 //{ Driver Code Starts
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class gfg {
-    public static void main(String[] args) throws Exception {
+public class GFG {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        int testCases = Integer.parseInt(br.readLine());
 
-        while (t-- > 0) {
-            int w = Integer.parseInt(br.readLine());
+        while (testCases-- > 0) {
+            // Read capacity
+            int capacity = Integer.parseInt(br.readLine());
 
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            List<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
+            // Read values
+            String[] valInput = br.readLine().split(" ");
+            int[] val = new int[valInput.length];
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
             }
 
-            int[] val = new int[array.size()];
-            int idx = 0;
-            for (int i : array) val[idx++] = i;
-
-            String lin = br.readLine();
-            String[] toke = lin.split(" ");
-            List<Integer> array1 = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : toke) {
-                array1.add(Integer.parseInt(token));
+            // Read weights
+            String[] wtInput = br.readLine().split(" ");
+            int[] wt = new int[wtInput.length];
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
             }
 
-            int[] wt = new int[array1.size()];
-            idx = 0;
-            for (int i : array1) wt[idx++] = i;
-
-            // calling method knapSack() of class Solution
-            System.out.println(new Solution().knapSack(w, wt, val));
+            // Call the knapSack function and print the result
+            System.out.println(Solution.knapSack(capacity, val, wt));
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    // Function to return max value that can be put in knapsack of capacity W.
-    static int knapSack(int W, int wt[], int val[]) {
+    // Function to return max value that can be put in knapsack of capacity.
+    static int knapSack(int capacity, int val[], int wt[]) {
         int n = wt.length;
-        int[][] dp = new int[n + 1][W + 1];
-        for (int[] dp1D : dp) {
-            Arrays.fill(dp1D, -1);
-        }
-        return solve(W, wt, val, n - 1, dp);
+        // return solveRecursion(n - 1, capacity, val, wt);
+        // int[][] dp = new int[n + 1][capacity + 1];
+        // for (int i = 0; i <= n; i++) {
+        //     for (int j = 0; j <= capacity; j++) {
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // return solveMemoization(n - 1, capacity, val, wt, dp);
+        return solveTabulation(capacity, val, wt);
     }
     
-    private static int solve(int W, int[] wt, int[] val, int index, int[][] dp) {
-        // base condition
-        if (index < 0) {
+    /**
+     * Using Tabulation
+     * 
+     * TC: O(N x W)
+     * SC: O(N x W)
+     */
+    private static int solveTabulation(int w, int val[], int wt[]) {
+        int n = wt.length;
+        // return solveRecursion(n - 1, capacity, val, wt);
+        int[][] dp = new int[n + 1][w + 1];
+        // base case in Recursion is converted to Initialization in Tabulation
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < w + 1; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < w + 1; j++) {
+                // convert index to (i - 1) and w to j
+                if (wt[i - 1] <= j) {
+                    dp[i][j] = 
+                        Math.max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][w];
+    }
+
+    /**
+     * Using Memoization
+     * 
+     * TC: O(N x W)
+     * SC: O(N x W + N)
+     */
+    private static int solveMemoization(int index, int w, int val[], int wt[], int[][] dp) {
+        // base case - minimum valid testcase
+        if (w == 0) {
             return 0;
         }
-        if (dp[index][W] != -1) {
-            return dp[index][W];
+        if (index == 0) {
+            return wt[0] <= w ? val[0] : 0;
         }
-        // pick
-        int x = 0;
-        if (W - wt[index] >= 0) {
-            x = val[index] + solve(W - wt[index], wt, val, index - 1, dp);
+        if (dp[index][w] != -1) {
+            return dp[index][w];
         }
-        // not pick
-        int y = solve(W, wt, val, index - 1, dp);
-        dp[index][W] = Math.max(x, y);
-        return dp[index][W];
+        /**
+         * if wt[index] <= w, i.e current weight is less than 
+         * capacity 'w' then we have two choices 
+         * 1. to pick
+         * 2. not to pick
+         * 
+         * so we need to take maximum of them to fill the knapsack
+         */
+        if (wt[index] <= w) {
+            return dp[index][w] = 
+                Math.max(
+                    val[index] + solveMemoization(index - 1, w - wt[index], val, wt, dp),
+                    solveMemoization(index - 1, w, val, wt, dp)
+                );
+        } else {
+            // we have no option to pick this
+            return dp[index][w] = solveMemoization(index - 1, w, val, wt, dp);
+        }
+    }
+ 
+    /**
+     * Using Recursion
+     * 
+     * TC: O(2 ^ N)
+     * SC: O(2 ^ N)
+     */
+    private static int solveRecursion(int index, int w, int val[], int wt[]) {
+        // base case - minimum valid testcase
+        if (w == 0) {
+            return 0;
+        }
+        if (index == 0) {
+            return wt[0] <= w ? val[0] : 0;
+        }
+        /**
+         * if wt[index] <= w, i.e current weight is less than 
+         * capacity 'w' then we have two choices 
+         * 1. to pick
+         * 2. not to pick
+         * 
+         * so we need to take maximum of them to fill the knapsack
+         */
+        if (wt[index] <= w) {
+            return Math.max(
+                val[index] + solveRecursion(index - 1, w - wt[index], val, wt),
+                solveRecursion(index - 1, w, val, wt)
+            );
+        } else {
+            // we have no option to pick this
+            return solveRecursion(index - 1, w, val, wt);
+        }
     }
 }
