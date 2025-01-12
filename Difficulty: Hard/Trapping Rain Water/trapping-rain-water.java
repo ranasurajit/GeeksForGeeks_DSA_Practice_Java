da5b197bb05a7a66a1f5a19902f6e7de81348803
Surajit Rana <surajit1602@gmail.com>
2025-01-12 12:40:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -24,26 +24,33 @@ class Solution {
      * 
      * TC: O(3 x N) ~ O(N)
      * SC: O(2 x N) ~ O(N)
+     * 
+     * @param arr
+     * @return
      */
     public int maxWater(int arr[]) {
         int n = arr.length;
-        int[] prefix = new int[n];         // SC: O(N)
-        int[] suffix = new int[n];         // SC: O(N)
-        
-        prefix[0] = arr[0];
-        for (int i = 1; i < n; i++) {      // TC: O(N)
-            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        /**
+         * we need to calculate the maximum height of buildings
+         * from either sides for an index
+         */
+        int[] leftMax = new int[n]; // SC: O(N)
+        int[] rightMax = new int[n]; // SC: O(N)
+        // left to right
+        leftMax[0] = arr[0];
+        for (int i = 1; i < rightMax.length; i++) { // TC: O(N)
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
         }
-        
-        suffix[n - 1] = arr[n - 1];
+        // right to left
+        rightMax[n - 1] = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) { // TC: O(N)
-            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
         }
-        
-        int water = 0;
-        for (int i = 0; i < n; i++) {      // TC: O(N)
-            water += Math.min(prefix[i], suffix[i]) - arr[i];
+        // water collected at any index = Min(leftMax[i], rightMax[i]) - height[i]
+        int waterCollected = 0;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            waterCollected += Math.min(leftMax[i], rightMax[i]) - arr[i];
         }
-        return water;
+        return waterCollected;
     }
 }
