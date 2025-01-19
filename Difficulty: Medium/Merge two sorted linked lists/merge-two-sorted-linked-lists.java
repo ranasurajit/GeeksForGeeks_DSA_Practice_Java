215a1ccd16b06a8da65333e0af31014d3794b9a5
Surajit Rana <surajit1602@gmail.com>
2025-01-20 02:10:23 +0530

    Update merge-two-sorted-linked-lists.java

--- Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.java
@@ -118,5 +118,27 @@ class LinkedList
             temp.next = t1;
         }
         return dummy.next;
-    } 
+    }
+
+    /**
+     * Using Recursive Approach
+     * 
+     * TC: O(M + N)
+     * Sc: O(M + N)
+     */
+    Node sortedMerge(Node head1, Node head2) {
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+        if (head1.data < head2.data) {
+            head1.next = sortedMerge(head1.next, head2);
+            return head1;
+        } else {
+            head2.next = sortedMerge(head1, head2.next);
+            return head2;
+        }
+    }
 }
