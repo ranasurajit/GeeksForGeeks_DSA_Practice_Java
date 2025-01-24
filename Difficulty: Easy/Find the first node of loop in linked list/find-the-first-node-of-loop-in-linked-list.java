44c01f3e4af44308a92ae25f61690c46c88421bf
Surajit Rana <surajit1602@gmail.com>
2025-01-25 00:30:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void makeLoop(Node head, Node tail, int x) {
+        if (x == 0) return;
+
+        Node curr = head;
+        for (int i = 1; i < x; i++) curr = curr.next;
+
+        tail.next = curr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            makeLoop(head, tail, k);
+            Solution ob = new Solution();
+            Node ans = ob.findFirstNode(head);
+            System.out.println(ans == null ? -1 : ans.data);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    /**
+     * Using Two (Fast and Slow) Pointers Approach
+     * 
+     * TC: O(2 x N) ~ O(N)
+     * SC: O(N)
+     */
+    public static Node findFirstNode(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        // move the slow pointer by 1 step and fast pointer by 2 steps
+        while (fast != null && fast.next != null) { // TC: O(N)
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            // there is no loop
+            return null;
+        }
+        // the linked-list has a loop
+        // if slow and fast meets at head
+        if (slow == head && fast == head) {
+            return head;
+        }
+        // now set the fast pointer to head and move both slow and fast pointers by 1 step
+        fast = head;
+        while (fast != null) { // TC: O(N)
+            slow = slow.next;
+            fast = fast.next;
+            if (slow == fast) {
+                return slow;
+            }
+        }
+        return null;
+    }
+}
