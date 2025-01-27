71faf749b57153ca7aa828443f9468d63e2bf6d1
Surajit Rana <surajit1602@gmail.com>
2025-01-27 23:16:23 +0530

    Update lru-cache.java

--- Difficulty: Hard/LRU Cache/lru-cache.java
+++ Difficulty: Hard/LRU Cache/lru-cache.java
@@ -36,22 +36,104 @@ public class LRUDesign {
 }
 
 // } Driver Code Ends
-
-
 // design the class in the most optimal way
+/**
+ * Using HashMap and DoublyLinkedList
+ *
+ * TC: O(1)
+ * SC: O(1)
+ */
+class LRUCache {
+    private static Map<Integer, Node> map;
+    private static int capacity;
+    private static Node head;
+    private static Node tail;
+    
+    // Constructor for initializing the cache capacity with the given value.
+    LRUCache(int cap) {
+        this.map = new HashMap<Integer, Node>();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head.next = tail;
+        tail.prev = head;
+        this.capacity = cap;
+    }
+    
+    /**
+     * We will be pushing recent put/get values just after the Head Node
+     */
+    private static void insertAfterHead(Node node) {
+        Node headNext = head.next;
+        head.next = node;
+        node.next = headNext;
+        headNext.prev = node;
+        node.prev = head;
+        map.put(node.key, node);
+    }
+    
+    /**
+     * We will be deleting any Node from the DoublyLinkedList
+     */
+    private static void deleteNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        map.remove(node.key);
+    }
+
+    // Function to return value corresponding to the key.
+    public static int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node node = map.get(key);
+        // we need to remove it from LinkedList and insert it after Head Node
+        deleteNode(node);
+        insertAfterHead(node);
+        return node.val;
+    }
+
+    // Function for storing key-value pair.
+    public static void put(int key, int value) {
+        if (map.containsKey(key)) {
+            // we need to update the value
+            deleteNode(map.get(key));
+        } else {
+            if (map.size() == capacity) {
+                deleteNode(tail.prev);
+            }
+        }
+        Node newNode = new Node(key, value);
+        insertAfterHead(newNode);
+    }
+    
+    static class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        
+        public Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+}
+
 /**
  * Using HashMap and ArrayDeque
  *
  * TC: O(1)
  * SC: O(1)
  */
-class LRUCache {
+class LRUCacheDeque {
     private static ArrayDeque<Integer> deque = null;
     private static Map<Integer, Integer> map;
     private static int n = 0;
     
     // Constructor for initializing the cache capacity with the given value.
-    LRUCache(int cap) {
+    LRUCacheDeque(int cap) {
         map = new HashMap<Integer, Integer>();
         deque = new ArrayDeque<Integer>();
         n = cap;
@@ -81,3 +163,4 @@ class LRUCache {
         deque.addLast(key);
     }
 }
+
