a84ae8059f108bf80a46bc263c23d2ff7eca2c9a
Surajit Rana <surajit1602@gmail.com>
2025-01-29 12:45:37 +0530

    Update undirected-graph-cycle.java

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -34,29 +34,54 @@ class GFG {
 
 class Solution {
     // Function to detect cycle in an undirected graph.
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean[] visited = new boolean[V];
-        for (int i = 0; i < V; i++) {
-            if (!visited[i] && graphHasCycleBFS(adj, visited, i)) {
-                return true;
+    /**
+     * BFS Approach
+     * 
+     * TC: O(2 x V + 2 x E) ~ O(V + E)
+     * SC: O(2 x V) ~ O(V)
+     * 
+     * @param adj
+     * @return
+     */
+    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
+        int n = adj.size();
+        boolean[] visited = new boolean[n]; // SC: O(V)
+        for (int i = 0; i < n; i++) {       // TC: O(V)
+            if (!visited[i] && hasCycleInGraphBFS(i, adj, visited)) {
+                return true; // TC: O(V + 2 x E), SC: O(V)
             }
         }
         return false;
     }
     
-    private boolean graphHasCycleBFS(ArrayList<ArrayList<Integer>> adj,
-        boolean[] visited, int index) {
-        Queue<Pair> queue = new LinkedList<Pair>();
-        queue.offer(new Pair(index, -1));
-        visited[index] = true;
+    /**
+     * BFS Approach
+     * 
+     * TC: O(V + 2 x E)
+     * SC: O(V)
+     * 
+     * @param source
+     * @param adj
+     * @param visited
+     * @return
+     */
+    private static boolean hasCycleInGraphBFS(int source,
+        ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        // storing the (node, parent) in queue
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[] { source, -1 });
+        visited[source] = true;
         while (!queue.isEmpty()) {
-            Pair current = queue.poll();
-            int u = current.node;
-            int parent = current.parent;
+            int[] current = queue.poll();
+            int u = current[0];
+            int parent = current[1];
             for (Integer v : adj.get(u)) {
+                if (v == parent) {
+                    continue;
+                }
                 if (!visited[v]) {
+                    queue.offer(new int[] { v, u });
                     visited[v] = true;
-                    queue.offer(new Pair(v, u));
                 } else if (v != parent) {
                     return true;
                 }
@@ -64,14 +89,53 @@ class Solution {
         }
         return false;
     }
-    
-    class Pair {
-        int node;
-        int parent;
-        
-        public Pair (int node, int parent) {
-            this.node = node;
-            this.parent = parent;
+
+    /**
+     * DFS Approach
+     * 
+     * TC: O(2 x V + 2 x E) ~ O(V + E)
+     * SC: O(2 x V) ~ O(V)
+     * 
+     * @param adj
+     * @return
+     */
+    public boolean isCycleDFS(ArrayList<ArrayList<Integer>> adj) {
+        int n = adj.size();
+        boolean[] visited = new boolean[n]; // SC: O(V)
+        for (int i = 0; i < n; i++) { // TC: O(V)
+            if (!visited[i] && hasCycleInGraphDFS(i, -1, adj, visited)) {
+                return true; // TC: O(V + 2 x E), SC: O(V)
+            }
         }
+        return false;
+    }
+    
+    /**
+     * DFS Approach
+     * 
+     * TC: O(V + 2 x E)
+     * SC: O(V)
+     * 
+     * @param u
+     * @param parent
+     * @param adj
+     * @param visited
+     * @return
+     */
+    private boolean hasCycleInGraphDFS(int u, int parent,
+            ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        visited[u] = true;
+        for (Integer v : adj.get(u)) {
+            if (v == parent) {
+                continue;
+            }
+            if (visited[v]) {
+                return true;
+            }
+            if (hasCycleInGraphDFS(v, u, adj, visited)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
