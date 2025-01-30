e8b22923dd502d1b88a5e4c146ab2db7dc53fdd3
Surajit Rana <surajit1602@gmail.com>
2025-01-31 00:03:50 +0530

    Update cycle-in-a-directed-graph.java

--- Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.java
+++ Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.java
@@ -34,33 +34,41 @@ class DriverClass {
 
 class Solution {
     // Function to detect cycle in a directed graph.
+    /**
+     * Using DFS Approach
+     *
+     * TC: O(2 x V + E) ~ O(V + E)
+     * SC: O(V)
+     */
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean[] visited = new boolean[V];
-        boolean[] inRecursion = new boolean[V];
-        
-        for (int i = 0; i < V; i++) {
-            if (!visited[i] && hasDFSCycle(i, adj, visited, inRecursion)) {
-                return true;
-            }
+        boolean[] visited = new boolean[V]; // SC: O(V)
+        boolean[] inRecursion = new boolean[V]; // SC: O(V)
+
+        for (int i = 0; i < V; i++) { // TC: O(V)
+          if (!visited[i] && hasCycleDFS(i, adj, visited, inRecursion)) {
+            return true; // TC: O(V + E), SC: O(V)
+          }
         }
         return false;
     }
     
-    private boolean hasDFSCycle(int u, ArrayList<ArrayList<Integer>> adj,
+    /**
+     * TC: O(V + E)
+     * SC: O(V)
+     */
+    private static boolean hasCycleDFS(int u, ArrayList<ArrayList<Integer>> adj,
         boolean[] visited, boolean[] inRecursion) {
         visited[u] = true;
         inRecursion[u] = true;
-        
         for (Integer v : adj.get(u)) {
-            if (!visited[v] && hasDFSCycle(v, adj, visited, inRecursion)) {
-                return true;
-            } else if (inRecursion[v]) {
-                return true;
-            }
+          if (!visited[v] && hasCycleDFS(v, adj, visited, inRecursion)) {
+            return true;
+          } else if (inRecursion[v]) {
+            return true;
+          }
         }
-        
-        // Recuresion winded up so mark inRucursion at vertex u as false;
         inRecursion[u] = false;
         return false;
     }
 }
+
