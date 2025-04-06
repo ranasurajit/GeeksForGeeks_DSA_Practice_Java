8f194f9ab584581f38ded138c96029c0366cb12c
Surajit Rana <surajit1602@gmail.com>
2025-04-06 14:08:47 +0530

    Update topological-sort.java

--- Difficulty: Medium/Topological sort/topological-sort.java
+++ Difficulty: Medium/Topological sort/topological-sort.java
@@ -59,85 +59,76 @@ class Main {
 
 
 class Solution {
-    // Function to return list containing vertices in Topological order.
     /**
-     * Topological Sort using BFS (Kahn's Algorithm)
+     * Approach II : Using BFS Approach (Kahn's Algorithm)
      * 
-     * TC: O(3 x V + 2 x E) ~ O(V + E)
-     * SC: O(2 x V) ~ O(V)
-     * 
-     * @param adj
-     * @return
+     * TC: O(2 x (V + E)) ~ O(V + E)
+     * SC: O(3 x V + E) ~ O(V + E)
      */
-    static ArrayList<Integer> topologicalSort(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> topoList = new ArrayList<Integer>();
-        int n = adj.size();
-        // we need to calculate the indegrees of each vertices
-        int[] indegrees = new int[n]; // SC: O(V)
-        for (ArrayList<Integer> u : adj) { // TC: O(V)
-            for (Integer v : u) { // TC: O(E)
-                indegrees[v]++;
-            }
+    public static ArrayList<Integer> topoSort(int V, int[][] edges) {
+        ArrayList<Integer> sorted = new ArrayList<Integer>();
+        int[] indegrees = new int[V]; // SC: O(V)
+        Map<Integer, ArrayList<Integer>> adj =
+            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
+        for (int[] edge : edges) { // TC: O(E)
+            adj.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
+            indegrees[edge[1]]++;
         }
-        // Now we need to push the vertices with indegree value as 0 in the Queue
         Queue<Integer> queue = new LinkedList<Integer>(); // SC: O(V)
-        for (int i = 0; i < n; i++) { // TC: O(V)
+        for (int i = 0; i < V; i++) { // TC: O(V)
             if (indegrees[i] == 0) {
                 queue.offer(i);
             }
         }
-        // starting the BFS
         while (!queue.isEmpty()) { // TC: O(V + E)
             Integer u = queue.poll();
-            topoList.add(u);
-            for (Integer v : adj.get(u)) {
+            sorted.add(u);
+            for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
                 indegrees[v]--;
                 if (indegrees[v] == 0) {
                     queue.offer(v);
                 }
             }
         }
-        return topoList;
+        return sorted;
     }
 
     /**
-     * Topological Sort using DFS
+     * Approach I : Using DFS Approach
      * 
-     * TC: O(3 x V + E) ~ O(V + E)
-     * SC: O(3 x V) ~ O(V)
-     * 
-     * @param adj
-     * @return
+     * TC: O(3 x V + 2 x E) ~ O(V + E)
+     * SC: O(4 x V + E) ~ O(V + E)
      */
-    static ArrayList<Integer> topologicalSortDFS(ArrayList<ArrayList<Integer>> adj) {
-        ArrayList<Integer> topoList = new ArrayList<Integer>();
-        int v = adj.size();
-        boolean[] visited = new boolean[v];       // SC: O(V)
-        Stack<Integer> st = new Stack<Integer>(); // SC: O(V)
-        for (int i = 0; i < v; i++) {             // TC: O(V)
+    public static ArrayList<Integer> topoSortDFS(int V, int[][] edges) {
+        ArrayList<Integer> sorted = new ArrayList<Integer>();
+        Map<Integer, ArrayList<Integer>> adj =
+            new HashMap<Integer, ArrayList<Integer>>(); // SC: O(V + E)
+        for (int[] edge : edges) { // TC: O(E)
+            adj.computeIfAbsent(edge[0], k -> new ArrayList<Integer>()).add(edge[1]);
+        }
+        Stack<Integer> st = new Stack<Integer>(); // needed for topological sort - SC: O(V)
+        boolean[] visited = new boolean[V]; // SC: O(V)
+        for (int i = 0; i < V; i++) { // TC: O(V)
             if (!visited[i]) {
-                dfsGraph(i, adj, visited, st);    // TC: O(V + E), SC: O(V)
+                dfsGraph(i, adj, visited, st); // TC: O(V + E), SC: O(V)
             }
         }
-        while (!st.isEmpty()) {                   // TC: O(V)
-            topoList.add(st.pop());
+        while (!st.isEmpty()) { // TC: O(V)
+            sorted.add(st.pop());
         }
-        return topoList;
+        return sorted;
     }
     
     /**
+     * Using DFS Approach
+     * 
      * TC: O(V + E)
      * SC: O(V)
-     * 
-     * @param u
-     * @param adj
-     * @param visited
-     * @param st
      */
-    private static void dfsGraph(int u, ArrayList<ArrayList<Integer>> adj,
-            boolean[] visited, Stack<Integer> st) {
+    private static void dfsGraph(int u, Map<Integer, ArrayList<Integer>> adj, 
+        boolean[] visited, Stack<Integer> st) {
         visited[u] = true;
-        for (Integer v : adj.get(u)) {
+        for (Integer v : adj.getOrDefault(u, new ArrayList<Integer>())) {
             if (!visited[v]) {
                 dfsGraph(v, adj, visited, st);
             }
