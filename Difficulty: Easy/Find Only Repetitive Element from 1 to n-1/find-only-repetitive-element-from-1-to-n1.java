11de04fe5db54676769abb76eb53dafc56c7a1c0
Surajit Rana <surajit1602@gmail.com>
2025-04-20 01:20:33 +0530

    Update find-only-repetitive-element-from-1-to-n1.java

--- Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.java
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.java
@@ -31,12 +31,32 @@ class GFG {
 // User function Template for Java
 class Solution {
     /**
+     * Approach II : Using Bit-Manipulation Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int findDuplicate(int[] arr) {
+        int n = arr.length;
+        int xor = 0;
+        int xorNums = 0;
+
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            xor ^= arr[i];
+            if (i < n - 1) {
+                xorNums ^= (i + 1);
+            }
+        }
+        return xor ^ xorNums;
+    }
+
+    /**
      * Approach I : Using Hashing Approach
      * 
      * TC: O(2 x N) ~ O(N)
      * SC: O(L), where L = Max(arr))
      */
-    public int findDuplicate(int[] arr) {
+    public int findDuplicateApproachI(int[] arr) {
         int n = arr.length;
         int duplicate = 0;
         int maxValue = 0;
