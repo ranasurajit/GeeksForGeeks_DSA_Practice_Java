abfe17a2887939a52d487b29c413dd3fc125ed9d
Surajit Rana <surajit1602@gmail.com>
2025-04-24 02:52:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique Number III/unique-number-iii.java
+++ Difficulty: Medium/Unique Number III/unique-number-iii.java
@@ -28,12 +28,53 @@ public class Main {
 
 class Solution {
     /**
+     * Approach II : Using Bit-Manipulation Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
+    public int getSingle(int[] arr) {
+        // int n = arr.length;
+        // /**
+        //  * count stores the sum of all bits of numbers in array
+        //  * from rightmost bit to leftmost bit
+        //  * i.e. count is storing binary bits in reverse order
+        //  */
+        // int[] count = new int[32]; // SC: O(32) ~ O(1)
+        // for (int item : arr) { // TC: O(N)
+        //     int j = 0;
+        //     while (item > 0) { // TC: O(1)
+        //         int lastBit = (item & 1);
+        //         count[j] += lastBit;
+        //         j++;
+        //         item = item >> 1;
+        //     }
+        // }
+        // int unique = 0;
+        // int pow = 1;
+        // for (int i = 0; i < 32; i++) { // TC: O(32) ~ O(1)
+        //     count[i] = count[i] % 3; // removes contribution from 3 times of duplicates
+        //     unique += count[i] * pow;
+        //     pow = pow << 1;
+        // }
+        // return unique;
+        int ones = 0, twos = 0;
+
+        for (int num : arr) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
+        }
+
+        return ones;
+    }
+
+    /**
      * Approach I : Using Hashing Approach
      * 
      * TC: O(4 x (N / 3)) ~ O(N)
      * SC: O(N / 3) ~ O(N)
      */
-    public int getSingle(int[] arr) {
+    public int getSingleApproachI(int[] arr) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // TC: O(N / 3 + 1)
         for (int item : arr) { // TC: O(N)
             map.put(item, map.getOrDefault(item, 0) + 1);
