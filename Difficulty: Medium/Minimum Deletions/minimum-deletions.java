5fbfedd25623795f58ede6c173e5b5f2c99da1ff
Surajit Rana <surajit1602@gmail.com>
2025-05-22 13:13:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Deletions/minimum-deletions.java
+++ Difficulty: Medium/Minimum Deletions/minimum-deletions.java
@@ -1,11 +1,40 @@
 class Solution {
     /**
-     * Approach II : Using Memoization Approach
+     * Approach III : Using Tabulation Approach
      * 
-     * TC: O(4 ^ N + N) ~ O(4 ^ N)
-     * SC: O(2 x N) ~ O(N)
+     * TC: O(N ^ 2)
+     * SC: O(N ^ 2)
      */
     static int minDeletions(String s) {
+        int n = s.length();
+        /**
+         * we can find the characters which can form the longest palindrome
+         * so the question is reduced to finding length of longest common subsequence
+         */
+        String p = reverse(s); // TC: O(N)
+        // Initialization
+        int[][] dp = new int[n + 1][n + 1]; // SC: O(N ^ 2)
+        // by default dp[0][0], dp[i][0] and dp[0][j] = 0 as per Base Case
+        // Iterative Calls
+        for (int i = 1; i < n + 1; i++) { // TC: O(N)
+            for (int j = 1; j < n + 1; j++) { // TC: O(N)
+                dp[i][j] = s.charAt(i - 1) == p.charAt(j - 1) ? 
+                    1 + dp[i - 1][j - 1] : Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        int lcsLength = dp[n][n];
+        return n - lcsLength;
+    }
+
+    /**
+     * Approach II : Using Memoization Approach
+     * 
+     * TC: O(M x N) ~ O(N ^ 2)
+     * SC: O((M x N) + (M + N)) ~ O(N ^ 2 + N)
+     * 
+     * as M = N
+     */
+    static int minDeletionsMemoization(String s) {
         int n = s.length();
         /**
          * we can find the characters which can form the longest palindrome
@@ -23,10 +52,8 @@ class Solution {
     /**
      * Using Memoization Approach
      * 
-     * TC: O(2 ^ (M + N)) ~ O(4 ^ N)
-     * SC: O(M + N) ~ O(2 x N) ~ O(N)
-     * 
-     * as M = N
+     * TC: O(M x N)
+     * SC: O(M + N)
      */
     private static int lcsMemoization(String s1, String s2, int m, int n, int[][] memo) {
         // Base Case
