8164bd9cd14a5538e5f7fe18cbf8ed99ae746225
Surajit Rana <surajit1602@gmail.com>
2025-05-27 13:10:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print leaf nodes from preorder traversal of BST/print-leaf-nodes-from-preorder-traversal-of-bst.java
+++ Difficulty: Medium/Print leaf nodes from preorder traversal of BST/print-leaf-nodes-from-preorder-traversal-of-bst.java
@@ -9,24 +9,33 @@ class Solution {
         int n = preorder.length;
         ArrayList<Integer> leafs = new ArrayList<Integer>();
         Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
+        // push the root node into Stack as we know preorder stores the root at index '0'
         st.push(preorder[0]);
         for (int i = 1; i < n; i++) { // TC: O(N)
             if (preorder[i] < st.peek()) {
                 st.push(preorder[i]);
             } else {
+                // switch from left sub-tree to right sub-tree
                 int temp = st.pop();
+                // count of nodes after there is a switch from left sub-tree to right sub-tree
                 int count = 1;
                 while (!st.isEmpty() && preorder[i] > st.peek()) {
                     st.pop();
                     count++;
                 }
+                // for temp node to be leaf count should be 2 or more
+                // if count <= 1 then temp node will be root of the sub-tree and not the leaf
                 if (count > 1) {
                     leafs.add(temp);
                 }
                 st.push(preorder[i]);
             }
         }
-        leafs.add(preorder[n - 1]);
+        /**
+         * lastly added node to Stack is also a leaf node and 
+         * denotes a leaf node at the right sub-tree
+         */
+        leafs.add(st.pop());
         return leafs;
     }
 }
