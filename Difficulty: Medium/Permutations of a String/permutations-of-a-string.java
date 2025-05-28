44428ee82449792c154c96ab88db1ee879841f55
Surajit Rana <surajit1602@gmail.com>
2025-05-29 00:16:36 +0530

    Update permutations-of-a-string.java

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
@@ -28,84 +28,100 @@ class GFG {
 
 
 class Solution {
-    public ArrayList<String> findPermutation(String s) {
-        Set<String> result = new HashSet<String>();
-        StringBuilder sb = new StringBuilder();
-        solveSwap(0, s, sb, result);
-        return new ArrayList<>(result);
-    }
-    
     /**
-     * Approach II - Using Swapping
+     * Approach II : Using Recursion + Backtracking (Optimal) Approach
      * 
      * TC: O(N x N!)
-     * SC: O(N)
+     * SC: O(N x N! + 2 x N) ~ O(N x N!)
      * 
-     * @param s
-     * @return
+     * Accepted (Test Cases Passed: 1026 /1026)
      */
-    private void solveSwap(int idx, String s, StringBuilder sb, Set<String> result) {
-        // base case when sb.length() = s.length()
-        if (idx == s.length()) {
-            result.add(s);
-            return;
-        }
-        // we need to always begin from start index 0
-        for (int i = idx; i < s.length(); i++) {
-            // swap
-            s = swap(s, i, idx);
-            // explore
-            solveSwap(idx + 1, s, sb, result);
-            // backtrack
-            s = swap(s, i, idx);
-        }
-    }
-    
-    private String swap(String s, int fromIndex, int toIndex) {
-        char[] ch = s.toCharArray();
-        char temp = ch[toIndex];
-        ch[toIndex] = ch[fromIndex];
-        ch[fromIndex] = temp;
-        return String.valueOf(ch);
+    public ArrayList<String> findPermutation(String s) {
+        Set<String> resultSet = new HashSet<String>(); // SC: O(N)
+        Set<String> used = new HashSet<String>(); // SC: O(N)
+        backtrack(0, s.toCharArray(), used, resultSet); // TC: O(N x N!), SC: O(N x N! + N)
+        return new ArrayList<String>(resultSet);
     }
 
-    public ArrayList<String> findPermutationApproachI(String s) {
-        Set<String> result = new HashSet<String>();
-        Set<String> set = new HashSet<String>();
-        StringBuilder sb = new StringBuilder();
-        solve(s, sb, set, result);
-        return new ArrayList<>(result);
-    }
-    
     /**
-     * Approach I
+     * Using Recursion + Backtracking Approach
      * 
      * TC: O(N x N!)
-     * SC: O(N)
-     * 
-     * @param s
-     * @return
+     * SC: O(N x N! + N)
      */
-    private void solve(String s, StringBuilder sb, Set<String> set,
-            Set<String> result) {
-        // base case when sb.length() = s.length()
-        if (sb.length() == s.length()) {
-            result.add(sb.toString());
+    private void backtrack(int index, char[] chars, Set<String> used, Set<String> resultSet) {
+        // Base Case
+        if (index == chars.length) {
+            resultSet.add(String.valueOf(chars));
             return;
         }
-        // we need to always begin from start index 0
-        for (int i = 0; i < s.length(); i++) {
+        // Recursion Calls
+        for (int i = index; i < chars.length; i++) { // TC: O(N)
+            String key = chars[index] + "-" + index;
+            if (!used.contains(key)) {
+                used.add(key);
+                // swap the character at index with character at i
+                swap(chars, index, i);
+                backtrack(index + 1, chars, used, resultSet);
+                // backtrack
+                swap(chars, index, i); // undo the swap operation to explore other possibilities
+                used.remove(key);
+            }
+        }
+    }
+
+    /**
+     * Swapping Approach
+     * 
+     * TC: O(1)
+     * SC: O(1)
+     */
+    private void swap(char[] chars, int a, int b) {
+        char temp = chars[b];
+        chars[b] = chars[a];
+        chars[a] = temp;
+    }
+
+    /**
+     * Approach I : Using Recursion Approach
+     * 
+     * TC: O(N x N!)
+     * SC: O(N x N! + 4 x N) ~ O(N x N!)
+     * 
+     * Time limit exceeded (Test Cases Passed: 1015 /1026)
+     */
+    public ArrayList<String> findPermutationApproachI(String s) {
+        Set<String> resultSet = new HashSet<String>(); // SC: O(N)
+        Set<String> used = new HashSet<String>(); // SC: O(N)
+        StringBuilder sb = new StringBuilder(); // SC: O(N)
+        solveRecursion(s, sb, resultSet, used); // TC: O(N x N!), SC: O(N x N! + N)
+        return new ArrayList<String>(resultSet);
+    }
+
+    /**
+     * Using Recursion Approach
+     * 
+     * TC: O(N x N!)
+     * SC: O(N x N! + N)
+     */
+    private void solveRecursion(String s, StringBuilder sb, Set<String> resultSet,
+            Set<String> used) {
+        // Base Case
+        if (sb.length() == s.length()) {
+            resultSet.add(sb.toString());
+            return;
+        }
+        // Recursion Calls
+        for (int i = 0; i < s.length(); i++) { // TC: O(N)
             char ch = s.charAt(i);
             String key = ch + "-" + i;
-            if (!set.contains(key)) {
-                // choose option
+            if (!used.contains(key)) {
+                used.add(key);
                 sb.append(ch);
-                set.add(ch + "-" + i);
-                // explore further
-                solve(s, sb, set, result);
-                // backtrack
+                solveRecursion(s, sb, resultSet, used);
+                // backtrack sb and used to explore other paths/choices
                 sb.setLength(sb.length() - 1);
-                set.remove(ch + "-" + i);
+                used.remove(key);
             }
         }
     }
