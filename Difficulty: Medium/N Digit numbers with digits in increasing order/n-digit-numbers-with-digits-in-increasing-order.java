b82d8107d98a2c2a6a074dc3f34ea6c702a0541a
Surajit Rana <surajit1602@gmail.com>
2025-05-29 21:03:29 +0530

    Update n-digit-numbers-with-digits-in-increasing-order.java

--- Difficulty: Medium/N Digit numbers with digits in increasing order/n-digit-numbers-with-digits-in-increasing-order.java
+++ Difficulty: Medium/N Digit numbers with digits in increasing order/n-digit-numbers-with-digits-in-increasing-order.java
@@ -1,7 +1,6 @@
-
 class Solution {
     /**
-     * Approach : Using Recursion + Backtracking Approach
+     * Approach II : Using Recursion + Backtracking Approach
      * 
      * TC: O(9 ^ N)
      * SC: O(N)
@@ -12,9 +11,51 @@ class Solution {
             for (int i = 0; i <= 9; i++) { // TC: O(1)
                 result.add(i);
             }
-        } else {
-            solveBacktrack(0, -1, n, new StringBuilder(), result); // TC: O(9 ^ N), SC: O(N)
+            return result;
         }
+        int[] num = { 0 };
+        solve(0, n, num, result); // TC: O(9 ^ N), SC: O(N)
+        return result;
+    }
+    
+    /**
+     * Using Recursion + Backtracking Approach
+     * 
+     * TC: O(9 ^ N)
+     * SC: O(N)
+     */
+    private static void solve(int lastDigit, int n, int[] num, ArrayList<Integer> result) {
+        // Base Case
+        if (n == 0) {
+            result.add(num[0]);
+            return;
+        }
+        // Recursion Calls
+        for (int i = lastDigit + 1; i <= 9; i++) {
+            // modify
+            num[0] = num[0] * 10 + i;
+            // explore
+            solve(i, n - 1, num, result);
+            // backtrack
+            num[0] = num[0] / 10;
+        }
+    }
+
+    /**
+     * Approach I : Using Recursion + Backtracking Approach
+     * 
+     * TC: O(9 ^ N)
+     * SC: O(N)
+     */
+    public static ArrayList<Integer> increasingNumbersApproachI(int n) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (n == 1) {
+            for (int i = 0; i <= 9; i++) { // TC: O(1)
+                result.add(i);
+            }
+            return result;
+        }
+        solveBacktrack(0, -1, n, new StringBuilder(), result); // TC: O(9 ^ N), SC: O(N)
         return result;
     }
     
