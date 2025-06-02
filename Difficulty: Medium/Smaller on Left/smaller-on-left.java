43bf3551af1ab19e109add25ce6d22421e581755
Surajit Rana <surajit1602@gmail.com>
2025-06-02 23:05:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smaller on Left/smaller-on-left.java
+++ Difficulty: Medium/Smaller on Left/smaller-on-left.java
@@ -1,11 +1,43 @@
 class Solution {
     /**
+     * Approach II : Using Stack Approach
+     * 
+     * As we have inner loop j which is dependent on i as j starts backwards 
+     * from (i - 1) to 0 so we can reduce the time complexity to 
+     * Linear O(N) by using Stack data-structure
+     * 
+     * TC: O(N)
+     * SC: O(N)
+     */
+    public int[] leftSmaller(int[] arr) {
+        int n = arr.length;
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<Integer>(); // SC: O(N)
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (st.isEmpty()) {
+                nse[i] = -1;
+            } else {
+                while (!st.isEmpty() && arr[i] <= st.peek()) {
+                    st.pop();
+                }
+                if (st.isEmpty()) {
+                    nse[i] = -1;
+                } else {
+                    nse[i] = st.peek();
+                }
+            }
+            st.push(arr[i]);
+        }
+        return nse;
+    }
+
+    /**
      * Approach I : Using Brute-Force Approach
      * 
      * TC: O(N ^ 2)
      * SC: O(1)
      */
-    public int[] leftSmaller(int[] arr) {
+    public int[] leftSmallerBruteForce(int[] arr) {
         int n = arr.length;
         int[] nse = new int[n];
         nse[0] = -1;
