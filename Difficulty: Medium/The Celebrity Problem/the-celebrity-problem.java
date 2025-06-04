59fd9382caad8889392ad62789304649675e3774
Surajit Rana <surajit1602@gmail.com>
2025-06-05 02:42:44 +0530

    Update the-celebrity-problem.java

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -27,26 +27,67 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    // Function to find if there is a celebrity in the party or not.
+    /**
+     * Approach II : Using Two Pointers Approach
+     * 
+     * TC: O(N)
+     * SC: O(1)
+     */
     public int celebrity(int mat[][]) {
-        int v = mat.length;
-        int[] indegrees = new int[v];
-        int[] outdegrees = new int[v];
-        for (int i = 0; i < v; i++) {
-            for (int j = 0; j < v; j++) {
+        int n = mat.length;
+        int top = 0;
+        int bottom = n - 1;
+        
+        while (top < bottom) { // TC: O(N)
+            if (mat[top][bottom] == 1) {
+                // index 'top' cannot be celebrity
+                top++;
+            } else if (mat[bottom][top] == 1) {
+                // index 'bottom' cannot be celebrity
+                bottom--;
+            } else if (mat[top][bottom] == 0 && mat[bottom][top] == 0) {
+                // both 'top' and 'bottom' indices cannot be celebrity
+                top++;
+                bottom--;
+            }
+        }
+        if (top > bottom) {
+            return -1;
+        }
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            if (i == top) {
+                continue;
+            }
+            if (mat[top][i] == 0 && mat[i][top] == 1) {
+                // do nothing as we have the celebrity as top or bottom
+            } else {
+                return -1;
+            }
+        }
+        return top;
+    }
+
+    /**
+     * Approach I : Using Graph (Indegrees and Outdegrees) Approach
+     * 
+     * TC: O(N ^ 2 + N) ~ O(N ^ 2)
+     * SC: O(2 x N) ~ O(N)
+     */
+    public int celebrityUsingGraphs(int mat[][]) {
+        int n = mat.length;
+        int[] indegrees = new int[n];  // SC: O(N)
+        int[] outdegrees = new int[n]; // TC: O(N)
+        
+        for (int i = 0; i < n; i++) {     // TC: O(N)
+            for (int j = 0; j < n; j++) { // TC: O(N)
                 if (i != j && mat[i][j] == 1) {
                     indegrees[j]++;
                     outdegrees[i]++;
                 }
             }
         }
-        /**
-         * the node having outdegrees = 0 and indegrees = v - 1 is the celebrity
-         * as the celebrity is one who does not know anyone but everyone else knows
-         * the celebrity
-         */
-        for (int i = 0; i < v; i++) {
-            if (outdegrees[i] == 0 && indegrees[i] == v - 1) {
+        for (int i = 0; i < n; i++) {    // TC: O(N)
+            if (outdegrees[i] == 0 && indegrees[i] == n - 1) {
                 return i;
             }
         }
