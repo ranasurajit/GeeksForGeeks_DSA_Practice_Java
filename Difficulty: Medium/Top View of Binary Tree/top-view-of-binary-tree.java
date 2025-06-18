107710cf9c28b340f0def1d3f3e711c9db081f09
Surajit Rana <surajit1602@gmail.com>
2025-06-19 00:00:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -1,16 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for JAVA
-
-import java.io.*;
-import java.util.*;
-import java.util.LinkedList;
-import java.util.Queue;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
-
 /*
 class Node{
     int data;
@@ -30,159 +17,48 @@ class Solution {
     /**
      * Approach : Using BFS Approach
      * 
-     * TC: O(2 x N) ~ O(N)
+     * TC: O(N x log(N) + 2 x N) ~ O(N x log(N))
      * SC: O(2 x N) ~ O(N)
      */
     static ArrayList<Integer> topView(Node root) {
-        ArrayList<Integer> view = new ArrayList<Integer>();
-        if (root == null) {
-            return view;
-        }
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // SC: O(N)
-        Queue<Pair> queue = new LinkedList<Pair>(); // SC: O(N)
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        List<int[]> nodePair = new ArrayList<int[]>(); // SC: O(N)
+        Queue<Pair> queue = new LinkedList<Pair>();    // SC: O(N)
         queue.offer(new Pair(root, 0));
-        int minOffset = 0;
         while (!queue.isEmpty()) { // TC: O(N)
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                Pair pair = queue.poll();
-                Node current = pair.node;
-                int offset = pair.offset;
-                minOffset = Math.min(minOffset, offset);
-                if (!map.containsKey(offset)) {
-                    map.put(offset, current.data);
+                Pair current = queue.poll();
+                Node node = current.node;
+                int col = current.col;
+                nodePair.add(new int[] { node.data, col });
+                if (node.left != null) {
+                    queue.offer(new Pair(node.left, col - 1));
                 }
-                if (current.left != null) {
-                    queue.offer(new Pair(current.left, offset - 1));
-                }
-                if (current.right != null) {
-                    queue.offer(new Pair(current.right, offset + 1));
+                if (node.right != null) {
+                    queue.offer(new Pair(node.right, col + 1));
                 }
             }
         }
-        int size = map.size();
-        int count = 0;
-        int index = minOffset;
-        while (count < size) { // TC: O(N)
-            view.add(map.get(index));
-            index++;
-            count++;
+        nodePair.sort((int[] a, int[] b) -> a[1] - b[1]); // TC: O(N x log(N))
+        int prevCol = Integer.MIN_VALUE;
+        for (int[] node : nodePair) { // TC: O(N)
+            int col = node[1];
+            if (col != prevCol) {
+                path.add(node[0]);
+            }
+            prevCol = col;
         }
-        return view;
+        return path;
     }
-    
+
     static class Pair {
         Node node;
-        int offset;
+        int col;
         
-        public Pair (Node node, int offset) {
+        public Pair (Node node, int col) {
             this.node = node;
-            this.offset = offset;
+            this.col = col;
         }
     }
 }
-
-
-//{ Driver Code Starts.
-
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
-public class Tree {
-
-    static Node buildTree(String str) {
-
-        if (str.length() == 0 || str.charAt(0) == 'N') {
-            return null;
-        }
-
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.add(root);
-        // Starting from the second element
-
-        int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-
-            // Get the current node's value from the string
-            String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= ip.length) break;
-
-            currVal = ip[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-
-        return root;
-    }
-
-    static void printInorder(Node root) {
-        if (root == null) return;
-
-        printInorder(root.left);
-        System.out.print(root.data + " ");
-
-        printInorder(root.right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-
-            Solution ob = new Solution();
-
-            ArrayList<Integer> vec = ob.topView(root);
-            for (int x : vec) System.out.print(x + " ");
-            System.out.println();
-
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
