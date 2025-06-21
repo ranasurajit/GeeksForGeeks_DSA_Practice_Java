4dd6d2f4de33e8394bb5c802c19cf5d0a81c07d2
Surajit Rana <surajit1602@gmail.com>
2025-06-21 23:57:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.java
@@ -0,0 +1,32 @@
+/*Complete the function below
+Node is as follows:
+class Node{
+    int data;
+    Node left,right;
+
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}
+
+*/
+class Solution {
+    // Function to check whether all nodes of a tree have the value
+    // equal to the sum of their child nodes.
+    public static int isSumProperty(Node root) {
+        return hasChildrenSum(root) ? 1 : 0;
+    }
+    
+    private static boolean hasChildrenSum(Node root) {
+        // Base Case
+        if (root == null || (root.left == null && root.right == null)) {
+            return true;
+        }
+        int leftVal = root.left == null ? 0 : root.left.data;
+        int rightVal = root.right == null ? 0 : root.right.data;
+        return leftVal + rightVal == root.data &&
+            hasChildrenSum(root.left) && hasChildrenSum(root.right);
+    }
+}
