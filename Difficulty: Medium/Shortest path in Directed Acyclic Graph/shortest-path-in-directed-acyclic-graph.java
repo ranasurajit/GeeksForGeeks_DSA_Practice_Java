625a8506baa0e62042b2b7e3338da27598f262ac
Surajit Rana <surajit1602@gmail.com>
2025-07-18 00:28:24 +0530

    Update shortest-path-in-directed-acyclic-graph.java

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
@@ -1,82 +1,57 @@
-//{ Driver Code Starts
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class Main {
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		while (T-- > 0) {
-			int n = sc.nextInt();
-			int m = sc.nextInt();
-			int[][] edge = new int[m][3];
-			for (int i = 0; i < m; i++) {
-				edge[i][0] = sc.nextInt();
-				edge[i][1] = sc.nextInt();
-				edge[i][2] = sc.nextInt();
-			}
-			Solution obj = new Solution();
-			int res[] = obj.shortestPath(n, m,edge);
-			for (int i = 0; i < n; i++) {
-				System.out.print(res[i] + " ");
-			}
-			System.out.println();
-		}
-	}
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
+// User function Template for Java
 class Solution {
+    /**
+     * Approach : Using Dijkstra's Algorithm Approach
+     * 
+     * TC: O(E) + O(E x log(V)) + O(V) ~ O(E x log(V))
+     * SC: O(V + E) + O(V) + O(E) ~ O(V + E)
+     */
+    public int[] shortestPath(int V, int E, int[][] edges) {
+        Map<Integer, ArrayList<int[]>> adj = createGraph(edges); // TC: O(E), SC: O(V + E)
+        int[] minDist = new int[V]; // SC: O(V)
+        Arrays.fill(minDist, (int) 1e9);
+        int src = 0;
+        minDist[src] = 0; // as 0 is the src
+        // we will be storing <distance, node> in the Min-Heap
+        PriorityQueue<int[]> pq = 
+            new PriorityQueue<int[]>((p, q) -> p[0] - q[0]); // SC: O(E)
+        pq.offer(new int[] { 0, src }); // TC: O(1) as 0 element was present initially 
+        while (!pq.isEmpty()) { // TC: O(E)
+            int[] current = pq.poll();
+            int weight = current[0];
+            int u = current[1];
+            if (weight > minDist[u]) {
+                // no shortest path can be achieved further 
+                continue;
+            }
+            for (int[] ngbr : adj.getOrDefault(u, new ArrayList<int[]>())) { // TC: O(E)
+                int v = ngbr[0];
+                int edgeWeight = ngbr[1];
+                if (weight + edgeWeight < minDist[v]) {
+                    minDist[v] = weight + edgeWeight;
+                    pq.offer(new int[] { weight + edgeWeight, v }); // TC: O(log(V))
+                }
+            }
+        }
+        for (int i = 0; i < V; i++) { // TC: O(V)
+            if (minDist[i] == (int) 1e9) {
+                minDist[i] = -1;
+            }
+        }
+        return minDist;
+    }
 
-	public int[] shortestPath(int N,int M, int[][] edges) {
-		HashMap<Integer, ArrayList<Pair>> adj = createGraph(N, M, edges);
-		int[] minPath = new int[N];
-		Arrays.fill(minPath, (int) 1e9);
-		PriorityQueue<Pair> pq =
-		    new PriorityQueue<Pair>((Pair p, Pair q) -> p.dist - q.dist);
-		pq.offer(new Pair(0, 0));
-		minPath[0] = 0;
-		while (!pq.isEmpty()) {
-		    Pair current = pq.poll();
-		    int u = current.node;
-		    int dist = current.dist;
-		    for (Pair pair : adj.get(u)) {
-		        int edgeNode = pair.node;
-		        int edgeDist = pair.dist;
-		        if (dist + edgeDist < minPath[edgeNode]) {
-		            minPath[edgeNode] = dist + edgeDist;
-		            pq.offer(new Pair(edgeNode, dist + edgeDist));
-		        }
-		    }
-		}
-		for (int i = 0; i < N; i++) {
-		    if (minPath[i] == (int) 1e9) {
-		        minPath[i] = -1;
-		    }
-		}
-		return minPath;
-	}
-	
-	private HashMap<Integer, ArrayList<Pair>> createGraph(int n,int m, int[][] edges) {
-	    HashMap<Integer, ArrayList<Pair>> adj = new HashMap<Integer, ArrayList<Pair>>();
-	    for (int i = 0; i < n; i++) {
-	        adj.put(i, new ArrayList<Pair>());
-	    }
-	    for (int i = 0; i < m; i++) {
-	        adj.get(edges[i][0]).add(new Pair(edges[i][1], edges[i][2]));
-	    }
-	    return adj;
-	}
-	
-	class Pair {
-	    int node;
-	    int dist;
-	    public Pair(int node, int dist) {
-	        this.node = node;
-	        this.dist = dist;
-	    }
-	}
+    /**
+     * Using Hashing Approach
+     * 
+     * TC: O(E)
+     * SC: O(V + E)
+     */
+    private Map<Integer, ArrayList<int[]>> createGraph(int[][] edges) {
+        Map<Integer, ArrayList<int[]>> adj = new HashMap<Integer, ArrayList<int[]>>();
+        for (int[] edge : edges) { // TC: O(E)
+            adj.computeIfAbsent(edge[0], k -> new ArrayList<int[]>()).add(new int[] { edge[1], edge[2] });
+        }
+        return adj;
+    }
 }
