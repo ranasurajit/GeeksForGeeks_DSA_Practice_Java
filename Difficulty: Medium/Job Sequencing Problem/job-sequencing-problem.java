e9a89e0f3d48d663ef2f8956572d4926ef0af138
Surajit Rana <surajit1602@gmail.com>
2025-07-27 00:16:12 +0530

    Update job-sequencing-problem.java

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
@@ -1,94 +1,39 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Job {
-    int id, profit, deadline;
-    Job(int x, int y, int z){
-        this.id = x;
-        this.deadline = y;
-        this.profit = z; 
-    }
-}
-
-class GfG {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        //testcases
-		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0){
-            String inputLine[] = br.readLine().trim().split(" ");
-            
-            //size of array
-            int n = Integer.parseInt(inputLine[0]);
-            Job[] arr = new Job[n];
-            inputLine = br.readLine().trim().split(" ");
-            
-            //adding id, deadline, profit
-            for(int i=0, k=0; i<n; i++){
-                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
-            }
-            
-            Solution ob = new Solution();
-            
-            //function call
-            int[] res = ob.JobScheduling(arr, n);
-            System.out.println (res[0] + " " + res[1]);
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     /**
      * Approach : Using Greedy Approach
      * 
-     * TC: O(N + N x log(N) + N x K) ~ O(N x log(N) + N x K)
-     * SC: O(2 x N + K) ~ O(N + K)
-     * 
-     * where K = average of deadline of N deadlines
+     * TC: O(N) + O(N x log(N)) + O(N) ~ O(N x log(N))
+     * SC: O(2 x N) ~ O(N)
      */
     public ArrayList<Integer> jobSequencing(int[] deadline, int[] profit) {
         int n = deadline.length;
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        int[][] sequence = new int[n][2]; // SC: O(2 x N)
-        int maxDeadline = -1;
+        int[][] jobs = new int[n][2]; // SC: O(2 x N)
+        int maxDeadline = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) { // TC: O(N)
-            sequence[i] = new int[] { deadline[i], profit[i] };
+            jobs[i] = new int[] { deadline[i], profit[i] };
             maxDeadline = Math.max(maxDeadline, deadline[i]);
         }
-        // sort the sequence in descending order of profit
-        Arrays.sort(sequence, (a, b) -> b[1] - a[1]); // TC: O(N x log(N))
-        int jobsCount = 0;
-        int totalProfit = 0;
-        int[] slot = new int[maxDeadline + 1]; // SC: O(K)
-        Arrays.fill(slot, -1);
+        // sorting the jobs in descending order of profits
+        Arrays.sort(jobs, (a, b) -> b[1] - a[1]); // TC: O(N x log(N))
+        int countJobs = 0;
+        int totalProfits = 0;
+        int[] slots = new int[maxDeadline + 1];
+        Arrays.fill(slots, -1);
         for (int i = 0; i < n; i++) { // TC: O(N)
-            for (int j = sequence[i][0]; j >= 1; j--) { // TC: O(K)
-                if (slot[j] == -1) {
-                    jobsCount++;
-                    slot[j] = i;
-                    totalProfit += sequence[i][1];
+            // deadline of ith job can go from jobs[i][0] to 1
+            for (int j = jobs[i][0]; j >= 1; j--) {
+                if (slots[j] == -1) {
+                    // fill the slot
+                    slots[j] = i;
+                    countJobs++;
+                    totalProfits += jobs[i][1];
                     break;
                 }
             }
         }
-        result.add(jobsCount);
-        result.add(totalProfit);
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        result.add(countJobs);
+        result.add(totalProfits);
         return result;
     }
 }
-
-/*
-class Job {
-    int id, profit, deadline;
-    Job(int x, int y, int z){
-        this.id = x;
-        this.deadline = y;
-        this.profit = z; 
-    }
-}
-*/
