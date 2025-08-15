dbbc38581d33770dfa8011fc6e79ba6915ac5a13
Surajit Rana <surajit1602@gmail.com>
2025-08-15 21:21:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.java
+++ Difficulty: Hard/Search Pattern (Rabin-Karp Algorithm)/search-pattern-rabin-karp-algorithm.java
@@ -1,71 +1,70 @@
 class Solution {
+    private static final int MOD = (int) 1e9 + 7;
+    
     /**
      * Approach : Using Rabin-Karp Algorithm Approach
      * 
-     * TC: O(M + N)
-     * SC: O(M)
+     * TC: O(N) + O(N) + O(M - N) + O(N) ~ O(M + N) 
+     * SC: O(N)
      * 
-     * worst case TC: O(M x N) if there are more spurious hits because of weak Hash function
+     * while the worst case time complexity is still O(M x N)
+     * we need to mitigate worst time complexity by creating a 
+     * string Hash Function
      */
     public ArrayList<Integer> rabinKarp(String text, String pattern) {
         ArrayList<Integer> result = new ArrayList<Integer>();
-        int m = pattern.length();
-        int n = text.length();
-        // Invalid case
-        if (m > n) {
+        int m = text.length();
+        int n = pattern.length();
+        if (n > m) {
             return result;
         }
         /**
-         * Hash function here will have a Base = 26
-         * 
-         * Base ^ (m - 1) x (pattern[0] - 'a') + Base ^ (m - 2) x (pattern[1] - 'a') +
-         * ..... + Base ^ 0 x (pattern[m - 1] - 'a')
+         * We need to create a HashFunction to convert the 'pattern' and
+         * search the same Hash value in the similar length portion of
+         * String 'text' and keep checking - we call this Rolling Hash
          */
-        long base = 26L; // as we have all lowercase letters
-        int mod = (int) 1e9 + 7;
-        // pre-calculation powers for reducing complexity
-        long[] powers = new long[m]; // SC: O(M)
-        powers[0] = 1;
-        for (int i = 1; i < m; i++) { // TC: O(M)
-            powers[i] = (base * powers[i - 1]) % mod;
-        }
+        long base = 31L;
         /**
-         * we will find the HashValue of our String 'pattern' 
-         * and [0 - (m - 1)] substring of String 'text'
+         * We need to calculate powers of base for ease of calculation
          */
+        long[] powers = new long[n]; // SC: O(N)
+        powers[0] = 1L;
+        for (int i = 1; i < n; i++) { // TC: O(N)
+            powers[i] = (base * powers[i - 1]) % MOD;
+        }
         long pHash = 0L;
         long tHash = 0L;
-        for (int i = 0; i < m; i++) { // TC: O(M)
-            pHash = (pHash * base + (pattern.charAt(i) - 'a')) % mod;
-            tHash = (tHash * base + (text.charAt(i) - 'a')) % mod;
+        for (int i = 0; i < n; i++) { // TC: O(N)
+            pHash = ((pHash * base) % MOD + (pattern.charAt(i) - 'a')) % MOD;
+            tHash = ((tHash * base) % MOD + (text.charAt(i) - 'a')) % MOD;
         }
-        for (int i = 0; i < n - m + 1; i++) { // TC: O(N - M)
-            if (pHash == tHash && isMatched(text, pattern, i, m, n)) { // TC: O(M)
+        for (int i = 0; i < m - n + 1; i++) { // TC: O(M - N)
+            if (pHash == tHash && isMatched(text, pattern, i, n)) { // TC: O(N)
                 result.add(i);
             }
-            if (i + m < n) {
-                /**
-                 * recalculating the tHash based on next upcoming index and 
-                 * negating calculations from index 'i'
-                 */
-                long removed = (powers[m - 1] * (text.charAt(i) - 'a')) % mod;
-                tHash = (tHash - removed + mod) % mod;
-                tHash = ((tHash * base) % mod + (text.charAt(i + m) - 'a')) % mod;
+            /**
+             * we need to remove calculation from index 'i' from tHash 
+             * so that next tHash can be calculated with index 'i + n'
+             */
+            if (i + n < m) {
+                long removed = (powers[n - 1] * (text.charAt(i) - 'a')) % MOD;
+                tHash = (tHash - removed + MOD) % MOD;
+                tHash = ((tHash * base) % MOD + (text.charAt(i + n) - 'a')) % MOD;
             }
         }
         return result;
     }
-
+    
     /**
      * Using Two Pointers Approach
      * 
-     * TC: O(M)
+     * TC: O(N)
      * SC: O(1)
      */
-    private boolean isMatched(String text, String pattern, int start, int m, int n) {
-        int p = 0; // start pointer of String 'pattern'
-        int q = start;
-        while (p < m && q < n) {
+    private boolean isMatched(String text, String pattern, int start, int n) {
+        int p = 0; // pointer at the start of String pattern
+        int q = start; // pointer at the start of String text
+        while (p < n) { // TC: O(N)
             if (pattern.charAt(p) != text.charAt(q)) {
                 return false;
             }
