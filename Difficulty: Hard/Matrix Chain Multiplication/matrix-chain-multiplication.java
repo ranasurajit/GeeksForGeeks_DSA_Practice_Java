d20d13e69c1d59ae8033946d4757cfe09f12f99f
Surajit Rana <surajit1602@gmail.com>
2025-08-19 12:29:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -1,35 +1,16 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().matrixMultiplication(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     /**
-     * Approach II : Using Memoization Approach
+     * Approach II : Using Memoization (Top-Down DP) Approach
      * 
-     * TC: O(N x N)
-     * SC: O(N x N + N)
+     * TC: O(N x 2 ^ N)
+     * SC: O(N)
+     * 
+     * Time Limit Exceeded (1110 / 1111 testcases passed)
      */
     static int matrixMultiplication(int arr[]) {
         int n = arr.length;
-        int[][] memo = new int[n + 1][n + 1]; // SC: O(N x N)
+        // mat[i] = arr[i] x arr[i - 1] so i can start from 1 to (n - 1)
+        int[][] memo = new int[n][n];
         for (int[] mem : memo) {
             Arrays.fill(mem, -1);
         }
@@ -37,61 +18,66 @@ class Solution {
     }
     
     /**
-     * Using Memoization
+     * Using Recursion Approach
      * 
-     * TC: O(N x N)
+     * TC: O(N x 2 ^ N)
      * SC: O(N)
      */
     private static int solveMemoization(int i, int j, int[] arr, int[][] memo) {
         // Base Case
         if (i >= j) {
+            // i cannot cross j, i cannot be equal to j as we need arr[i] and arr[i - 1] for i
             return 0;
         }
-        // Memoization Check
+        // Memoization
         if (memo[i][j] != -1) {
             return memo[i][j];
         }
         // Recursion Calls
-        int minOperation = Integer.MAX_VALUE;
-        for (int k = i; k <= j - 1; k++) {
-            int currentOperation = 
-                solveMemoization(i, k, arr, memo) + solveMemoization(k + 1, j, arr, memo) +
-                arr[i - 1] * arr[k] * arr[j];
-            minOperation = Math.min(minOperation, currentOperation);
+        int minMult = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) { // TC: O(N)
+            // currentMult = Operation(i, k) + Operation(k + 1, j) + Arr[i - 1] x Arr[k] x Arr[j]
+            int currentMult = solveMemoization(i, k, arr, memo) + 
+                solveMemoization(k + 1, j, arr, memo) + arr[i - 1] * arr[k] * arr[j];
+            minMult = Math.min(minMult, currentMult);
         }
-        return memo[i][j] = minOperation;
+        return memo[i][j] = minMult;
     }
 
     /**
      * Approach I : Using Recursion Approach
      * 
-     * TC: O(2 ^ N)
+     * TC: O(N x 2 ^ N)
      * SC: O(N)
+     * 
+     * Time Limit Exceeded (1110 / 1111 testcases passed)
      */
     static int matrixMultiplicationRecursion(int arr[]) {
         int n = arr.length;
+        // mat[i] = arr[i] x arr[i - 1] so i can start from 1 to (n - 1)
         return solveRecursion(1, n - 1, arr);
     }
     
     /**
-     * Using Recursion
+     * Using Recursion Approach
      * 
-     * TC: O(2 ^ N)
+     * TC: O(N x 2 ^ N)
      * SC: O(N)
      */
     private static int solveRecursion(int i, int j, int[] arr) {
         // Base Case
         if (i >= j) {
+            // i cannot cross j, i cannot be equal to j as we need arr[i] and arr[i - 1] for i
             return 0;
         }
         // Recursion Calls
-        int minOperation = Integer.MAX_VALUE;
-        for (int k = i; k <= j - 1; k++) {
-            int currentOperation = 
-                solveRecursion(i, k, arr) + solveRecursion(k + 1, j, arr) +
-                arr[i - 1] * arr[k] * arr[j];
-            minOperation = Math.min(minOperation, currentOperation);
+        int minMult = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) { // TC: O(N)
+            // currentMult = Operation(i, k) + Operation(k + 1, j) + Arr[i - 1] x Arr[k] x Arr[j]
+            int currentMult = solveRecursion(i, k, arr) + 
+                solveRecursion(k + 1, j, arr) + arr[i - 1] * arr[k] * arr[j];
+            minMult = Math.min(minMult, currentMult);
         }
-        return minOperation;
+        return minMult;
     }
 }
