3c575f05abb918245a5964d9368f2f8e109fa37c
Surajit Rana <surajit1602@gmail.com>
2025-08-20 12:11:26 +0530

    Update search-in-fully-rotated-sorted-2d-matrix.java

--- Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.java
+++ Difficulty: Medium/Search in fully rotated sorted 2D matrix/search-in-fully-rotated-sorted-2d-matrix.java
@@ -1,11 +1,52 @@
 class Solution {
     /**
-     * Approach : Using Binary Search Approach
+     * Approach II : Using Binary Search on 2D Matrix Approach
+     * 
+     * TC: O(log(N x M))
+     * SC: O(1)
+     */
+    public boolean searchMatrix(int[][] mat, int x) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int low = 0;
+        int high = n * m - 1;
+        while (low <= high) { // TC: O(log(N x M))
+            int mid = low + (high - low) / 2;
+            // converting 1D indices to 2D indices for mid
+            int rowMid = mid / m;
+            int colMid = mid % m;
+            int midVal = mat[rowMid][colMid];
+            // converting 1D indices to 2D indices for low and high
+            int lowVal = mat[low / m][low % m];
+            int highVal = mat[high / m][high % m];
+            if (midVal == x) {
+                return true;
+            } else if (midVal >= lowVal) {
+                // left side is sorted
+                if (x >= lowVal && x < midVal) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                // right side is sorted
+                if (x > midVal && x <= highVal) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Approach I : Using Binary Search By Rows Approach
      * 
      * TC: O(N x log(M))
      * SC: O(1)
      */
-    public boolean searchMatrix(int[][] mat, int x) {
+    public boolean searchMatrixBinarySearchByRows(int[][] mat, int x) {
         int n = mat.length;
         int m = mat[0].length;
         for (int i = 0; i < n; i++) { // TC: O(N)
